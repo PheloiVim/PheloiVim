449337b4aeafd92600f85f01e72f7499195e6cf5
yeuxacucodon <chiendien2006@gmail.com>
2022-12-04 14:46:53 +0700

    update keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,5 +7,15 @@ keymap("", "<F3>", ":NvimTreeFocus<cr>", opts)
 
 keymap("", "<F5>", ":ToggleTerm<cr>", opts)
 
-keymap("", "w", ":HopWord<cr>", opts)
-keymap("", "l", ":HopLine<cr>", opts)
+keymap("", "1", ":HopWord<cr>", opts)
+keymap("", "2", ":HopLine<cr>", opts)
+
+keymap("n", "<C-Up>", ":resize -2<CR>", opts)
+keymap("n", "<C-Down>", ":resize +2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
