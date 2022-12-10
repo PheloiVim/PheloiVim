a7d11a4d99cb2ec23bc466c92e3b19f4b2e26aa4
yeuxacucodon <chiendien2006@gmail.com>
2022-12-10 22:07:13 +0700

    update null-ls

--- lua/lsp/null-ls.lua
+++ lua/lsp/null-ls.lua
@@ -1,43 +1,17 @@
-local status, null_ls = pcall(require, "null-ls")
-if (not status) then return end
-
-local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-
-local lsp_formatting = function(bufnr)
-    vim.lsp.buf.format({
-        filter = function(client)
-            return client.name == "null-ls"
-        end,
-        bufnr = bufnr,
-    })
+local null_ls_status_ok, null_ls = pcall(require, "null-ls")
+if not null_ls_status_ok then
+	return
 end
 
-null_ls.setup {
-    sources = {
-        null_ls.builtins.formatting.prettierd,
-        null_ls.builtins.diagnostics.eslint_d.with({
-            diagnostics_format = '[eslint] #{m}\n(#{c})'
-        }),
-        null_ls.builtins.diagnostics.fish
-    },
-    on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                group = augroup,
-                buffer = bufnr,
-                callback = function()
-                    lsp_formatting(bufnr)
-                end,
-            })
-        end
-    end
-}
+local formatting = null_ls.builtins.formatting
+local diagnostics = null_ls.builtins.diagnostics
 
-vim.api.nvim_create_user_command(
-    'DisableLspFormatting',
-    function()
-        vim.api.nvim_clear_autocmds({ group = augroup, buffer = 0 })
-    end,
-    { nargs = 0 }
-)
+null_ls.setup({
+	debug = false,
+	sources = {
+		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
+		formatting.black.with({ extra_args = { "--fast" } }),
+		formatting.stylua,
+        diagnostics.flake8
+	},
+})
