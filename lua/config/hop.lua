3aeaf26c80d4d4846137c8abe91f6571f824bd5e
Vo Quang Chien <chiendien2006@gmail.com>
2022-12-14 12:37:33 +0700

    update hop.nvim

--- lua/config/hop.lua
+++ lua/config/hop.lua
@@ -4,10 +4,6 @@ hop.setup({
 })
 
 local directions = require("hop.hint").HintDirection
-vim.keymap.set("", "f", function()
-	hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
-end, { remap = true })
-
 vim.keymap.set("", "F", function()
-	hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
+	hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
 end, { remap = true })
