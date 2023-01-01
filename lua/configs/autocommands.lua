ce397a4ced3e0ea4b8293309e5e67208fdce120d
Vo Quang Chien <chiendien2006@gmail.com>
2023-01-01 10:54:53 +0700

    update autocmd

---
+++ lua/configs/autocommands.lua
@@ -0,0 +1,77 @@
+-- Auto install packer.nvim
+local fn = vim.fn
+local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+
+if fn.empty(fn.glob(install_path)) > 0 then
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#1e222a" })
+    print "Cloning packer .."
+    fn.system { "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path }
+end
+
+-- Auto reload plugins
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost packer.lua source <afile> | PackerSync
+  augroup end
+]])
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+	pattern = { "qf", "help", "man", "lspinfo", "spectre_panel" },
+	callback = function()
+		vim.cmd([[
+      nnoremap <silent> <buffer> q :close<CR> 
+      set nobuflisted 
+    ]])
+	end,
+})
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+	pattern = { "gitcommit", "markdown" },
+	callback = function()
+		vim.opt_local.wrap = true
+		vim.opt_local.spell = true
+	end,
+})
+
+vim.cmd("autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif")
+
+vim.api.nvim_create_autocmd({ "VimResized" }, {
+	callback = function()
+		vim.cmd("tabdo wincmd =")
+	end,
+})
+
+vim.api.nvim_create_autocmd({ "CmdWinEnter" }, {
+	callback = function()
+		vim.cmd("quit")
+	end,
+})
+
+vim.api.nvim_create_autocmd({ "TextYankPost" }, {
+	callback = function()
+		vim.highlight.on_yank({ higroup = "Visual", timeout = 200 })
+	end,
+})
+
+vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+	pattern = { "*.java" },
+	callback = function()
+		vim.lsp.codelens.refresh()
+	end,
+})
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, {
+	callback = function()
+		vim.cmd("hi link illuminatedWord LspReferenceText")
+	end,
+})
+
+vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
+	callback = function()
+	local line_count = vim.api.nvim_buf_line_count(0)
+		if line_count >= 5000 then
+			vim.cmd("IlluminatePauseBuf")
+		end
+	end,
+})
