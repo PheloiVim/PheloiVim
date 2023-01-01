ef20bf8a368b902e4ef358bec3708abeef6f5e4e
Vo Quang Chien <chiendien2006@gmail.com>
2023-01-01 11:01:11 +0700

    update treesitter

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,13 @@
+local status_ok, configs = pcall(require, "nvim-treesitter.configs")
+if not status_ok then
+    return
+end
+
+configs.setup({
+    highlight = {
+        enable = true,
+    },
+    autopairs = {
+        enable = true,
+    },
+})
