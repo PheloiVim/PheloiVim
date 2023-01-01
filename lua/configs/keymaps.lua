e89ccbeced84558e1331d8d2dcc29631ea69e5d3
Vo Quang Chien <chiendien2006@gmail.com>
2023-01-01 10:55:14 +0700

    update keymaps

---
+++ lua/configs/keymaps.lua
@@ -0,0 +1,21 @@
+local opts = { noremap = true, silent = true }
+
+local keymap = vim.keymap.set
+
+-- Telescope
+keymap("", "<F2>", ":NvimTreeToggle<cr>", opts)
+keymap("", "<F3>", ":NvimTreeFocus<cr>", opts)
+
+-- Hop
+keymap("", "1", ":HopWord<cr>", opts)
+keymap("", "2", ":HopLine<cr>", opts)
+
+-- Telescope
+keymap("n", "<leader>ff", ":Telescope find_files<CR>", opts)
+keymap("n", "<leader>ft", ":Telescope live_grep<CR>", opts)
+keymap("n", "<leader>fp", ":Telescope projects<CR>", opts)
+keymap("n", "<leader>fb", ":Telescope buffers<CR>", opts)
+
+-- Comment
+keymap("n", "<leader>/", "<cmd>lua require('Comment.api').toggle.linewise.current()<CR>", opts)
+keymap("x", "<leader>/", '<ESC><CMD>lua require("Comment.api").toggle.linewise(vim.fn.visualmode())<CR>')
