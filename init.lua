600c790adbde7d553071438e754709e61ac338eb
Vo Quang Chien <chiendien2006@gmail.com>
2023-01-01 10:53:10 +0700

    list require file

---
+++ init.lua
@@ -0,0 +1,25 @@
+-- Configs
+require "configs.autocommands"
+require "configs.keymaps"
+require "configs.options"
+require "configs.theme"
+
+-- Plugins
+require "plugins.alpha"
+require "plugins.autopairs"
+require "plugins.bufferline"
+require "plugins.cmp"
+require "plugins.comment"
+require "plugins.gitsigns"
+require "plugins.hop"
+require "plugins.identline"
+require "plugins.impatient"
+require "plugins.lualine"
+require "plugins.nvim_tree"
+require "plugins.packer"
+require "plugins.telescope"
+require "plugins.treesitter"
+
+-- LSP
+require "lsp.mason"
+require "lsp.null_ls"
