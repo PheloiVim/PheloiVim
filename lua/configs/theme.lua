874fee96ac5baf5e47f147fe68f9d4f3f47250e4
Vo Quang Chien <chiendien2006@gmail.com>
2023-01-01 19:34:48 +0700

    update highlight

--- lua/configs/theme.lua
+++ lua/configs/theme.lua
@@ -41,3 +41,37 @@ nightfox.setup({
 
 -- setup must be called before loading
 vim.cmd("colorscheme carbonfox")
+
+vim.api.nvim_set_hl(0, 'CursorLine', { bg = '#1b1f27' })
+
+local hl = vim.api.nvim_set_hl
+
+-- Nvim CMP
+hl(0, 'CmpDocBorder', { fg = '#1b1f27' })
+hl(0, 'CmPmenu', { bg = '#1e222a' })
+hl(0, 'CmpBorder', { fg = '#1b1f27' })
+hl(0, 'CmpItemAbbr', { fg = '#cdcecf' })
+hl(0, 'CmpItemAbbrMatch', { fg = '#dbc074', bold = true })
+hl(0, 'CmpItemKindConstant', { fg = '#fe9373' })
+hl(0, 'CmpItemKindFunction', { fg = '#86abdc' })
+hl(0, 'CmpItemKindSnippet', { fg = '#c94f6d' })
+hl(0, 'CmpItemKindIdentifier', { fg = '#e26886' })
+hl(0, 'CmpItemKindField', { fg = '#baa1e2' })
+hl(0, 'CmpItemKindVariable', { fg = '#9d79d6' })
+hl(0, 'CmpItemKindText', { fg = '#8ebaa4' })
+hl(0, 'CmpItemKindStructure', { fg = '#9d79d6' })
+hl(0, 'CmpItemKindType', { fg = '#dbc074' })
+hl(0, 'CmpItemKindKeyword', { fg = '#ced6e3' })
+hl(0, 'CmpItemKindMethod', { fg = '#86abdc' })
+-- LSP
+hl(0, 'DiagnosticHint', { fg = '#008080' })
+hl(0, 'DiagnosticError', { fg = '#ec5f67' })
+hl(0, 'LspSignatureActiveParameter', { fg = '#192330', bg = '#8ebaa4' })
+-- Float Windows
+hl(0, 'NormalFloat', { bg = '#161616' })
+hl(0, 'FloatBorder', { fg = '#61afef' })
+-- Git
+hl(0, 'DiffAdd', {fg = '#61afef'})
+hl(0, 'DiffAdded', {fg = '#98c379'})
+
+vim.api.nvim_command("redraw")
