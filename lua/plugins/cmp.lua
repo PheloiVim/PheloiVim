06e6fe05fb892a2865c2769833f85cb86ee22194
Vo Quang Chien <chiendien2006@gmail.com>
2023-01-01 11:00:21 +0700

    update cmp

---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,168 @@
+local cmp_status_ok, cmp = pcall(require, "cmp")
+if not cmp_status_ok then
+    return
+end
+
+local snip_status_ok, luasnip = pcall(require, "luasnip")
+if not snip_status_ok then
+    return
+end
+
+luasnip.config.set_config {
+    history = true,
+    updateevents = "TextChanged, TextChangedI"
+}
+
+local check_backspace = function()
+    local col = vim.fn.col "." - 1
+    return col == 0 or vim.fn.getline("."):sub(col, col):match "%s"
+end
+
+vim.api.nvim_create_autocmd("InsertLeave", {
+    callback = function()
+        if luasnip.session.current_nodes[vim.api.nvim_get_current_buf()]
+            and not luasnip.session.jump_active
+        then
+            luasnip.unlink_current()
+        end
+    end,
+})
+
+require("luasnip/loaders/from_vscode").lazy_load()
+
+local lsp_icons = {
+    Namespace = "",
+    Text = " ",
+    Method = " ",
+    Function = "",
+    Constructor = " ",
+    Field = "ﰠ ",
+    Variable = " ",
+    Class = "ﴯ ",
+    Interface = " ",
+    Module = " ",
+    Property = "ﰠ ",
+    Unit = "塞 ",
+    Value = " ",
+    Enum = " ",
+    Keyword = " ",
+    Snippet = " ",
+    Color = " ",
+    File = " ",
+    Reference = " ",
+    Folder = " ",
+    EnumMember = " ",
+    Constant = " ",
+    Struct = "פּ ",
+    Event = " ",
+    Operator = " ",
+    TypeParameter = " ",
+    Table = "",
+    Object = " ",
+    Tag = "",
+    Array = "[]",
+    Boolean = " ",
+    Number = " ",
+    Null = "ﳠ",
+    String = " ",
+    Calendar = "",
+    Watch = " ",
+    Package = "",
+    Copilot = " ",
+}
+
+local function border(hl_name)
+    return {
+        { "╭", hl_name },
+        { "─", hl_name },
+        { "╮", hl_name },
+        { "│", hl_name },
+        { "╯", hl_name },
+        { "─", hl_name },
+        { "╰", hl_name },
+        { "│", hl_name },
+    }
+end
+
+local cmp_window = require "cmp.utils.window"
+
+cmp_window.info_ = cmp_window.info
+cmp_window.info = function(self)
+    local info = self:info_()
+    info.scrollable = false
+    return info
+end
+
+local options = {
+    window = {
+        completion = {
+            border = border "CmpBorder",
+            winhighlight = "Normal:CmpPmenu,CursorLine:PmenuSel,Search:None",
+        },
+        documentation = {
+            border = border "CmpDocBorder",
+        },
+    },
+    snippet = {
+        expand = function(args)
+            luasnip.lsp_expand(args.body)
+        end,
+    },
+    formatting = {
+        format = function(_, vim_item)
+            vim_item.kind = string.format("%s %s", lsp_icons[vim_item.kind], vim_item.kind)
+            return vim_item
+        end,
+    },
+    mapping = cmp.mapping.preset.insert({
+        ["<C-k>"] = cmp.mapping.select_prev_item(),
+        ["<C-j>"] = cmp.mapping.select_next_item(),
+        ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
+        ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
+        ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+        ["<C-e>"] = cmp.mapping({
+            i = cmp.mapping.abort(),
+            c = cmp.mapping.close(),
+        }),
+        -- Accept currently selected item. If none selected, `select` first item.
+        -- Set `select` to `false` to only confirm explicitly selected items.
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif luasnip.expandable() then
+                luasnip.expand()
+            elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+            elseif check_backspace() then
+                fallback()
+            else
+                fallback()
+            end
+        end, {
+            "i",
+            "s",
+        }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, {
+            "i",
+            "s",
+        }),
+    }),
+    sources = {
+        { name = "luasnip" },
+        { name = "nvim_lsp" },
+        { name = "buffer" },
+        { name = "nvim_lua" },
+        { name = "path" },
+    },
+}
+
+cmp.setup(options)
