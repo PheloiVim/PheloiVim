866f3807a9a8a6beaabdfbc91977861f92cf32f2
Vo Quang Chien <chiendien2006@gmail.com>
2023-01-02 08:35:08 +0700

    fix null_ls format

--- lua/lsp/handlers.lua
+++ lua/lsp/handlers.lua
@@ -65,9 +65,7 @@ win.default_opts = function(options)
     return opts
 end
 
-M.on_attach = function(client, bufnr)
-    client.server_capabilities.documentFormattingProvider = true
-    client.server_capabilities.documentRangeFormattingProvider = false
+local function lsp_keymaps(bufnr)
     local opts = { noremap = true, silent = true }
     local keymap = vim.api.nvim_buf_set_keymap
     keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
@@ -76,7 +74,6 @@ M.on_attach = function(client, bufnr)
     keymap(bufnr, "n", "gI", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
     keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
     keymap(bufnr, "n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
-    keymap(bufnr, "n", "<leader>lf", "<cmd>lua vim.lsp.buf.format{ async = true }<cr>", opts)
     keymap(bufnr, "n", "<leader>li", "<cmd>LspInfo<cr>", opts)
     keymap(bufnr, "n", "<leader>lI", "<cmd>LspInstallInfo<cr>", opts)
     keymap(bufnr, "n", "<leader>la", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
@@ -85,6 +82,19 @@ M.on_attach = function(client, bufnr)
     keymap(bufnr, "n", "<leader>lr", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
     keymap(bufnr, "n", "<leader>ls", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
     keymap(bufnr, "n", "<leader>lq", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
+end
+
+M.on_attach = function(client, bufnr)
+    client.server_capabilities.documentFormattingProvider = false
+    client.server_capabilities.documentRangeFormattingProvider = false
+    lsp_keymaps(bufnr)
+    local bufcmd = vim.api.nvim_buf_create_user_command
+    local format = function()
+        local params = vim.lsp.util.make_formatting_params({})
+        client.request('textDocument/formatting', params, nil, bufnr)
+    end
+    bufcmd(bufnr, 'NullFormat', format, { desc = 'Format using null-ls' })
+    vim.keymap.set('n', '<leader>lf', '<cmd>NullFormat<cr>', { buffer = true })
     if client.server_capabilities.signatureHelpProvider then
         require("lsp.signature").setup(client)
     end
