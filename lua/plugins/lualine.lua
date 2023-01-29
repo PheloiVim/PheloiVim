50241da3f9b40c23904f2bede34265f7ce181f9e
yeuxacucodon <chiendien2006@gmail.com>
2023-01-29 21:18:43 +0700

    update

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,7 +3,58 @@ if not status_ok then
 	return
 end
 
-local function lsp_progress()
+local colors = {
+	yellow = "#ECBE7B",
+	red = "#ec5f67",
+	cyan = "#008080",
+}
+
+local file_info = function()
+	local icon = "  "
+	local filename = (vim.fn.expand("%") == "" and "Empty ") or vim.fn.expand("%:t")
+
+	if filename ~= "Empty " then
+		local devicons_present, devicons = pcall(require, "nvim-web-devicons")
+
+		if devicons_present then
+			local ft_icon = devicons.get_icon(filename)
+			icon = (ft_icon ~= nil and " " .. ft_icon) or ""
+		end
+
+		filename = " " .. filename .. " |"
+	end
+
+	return "%#St_file_info#" .. icon .. filename
+end
+
+local mode = {
+	"mode",
+	fmt = function(str)
+		return "-- " .. str .. " --"
+	end,
+}
+
+local location = {
+	"location",
+	padding = 1,
+}
+
+local git = function()
+	if not vim.b.gitsigns_head or vim.b.gitsigns_git_status then
+		return ""
+	end
+
+	local git_status = vim.b.gitsigns_status_dict
+
+	local added = (git_status.added and git_status.added ~= 0) and ("  " .. git_status.added) or ""
+	local changed = (git_status.changed and git_status.changed ~= 0) and ("  " .. git_status.changed) or ""
+	local removed = (git_status.removed and git_status.removed ~= 0) and ("  " .. git_status.removed) or ""
+	local branch_name = " " .. git_status.head .. " "
+
+	return "%#St_gitIcons#" .. branch_name .. added .. changed .. removed
+end
+
+local lsp_progress = function()
 	if not rawget(vim, "lsp") then
 		return ""
 	end
@@ -25,54 +76,38 @@ local function lsp_progress()
 	return ("%#St_LspProgress#" .. content) or ""
 end
 
-local function lsp()
+local diagnostic = {
+	"diagnostics",
+	sources = { "nvim_diagnostic" },
+	symbols = { error = " ", warn = " ", info = " " },
+	diagnostics_color = {
+		color_error = { fg = colors.red },
+		color_warn = { fg = colors.yellow },
+		color_info = { fg = colors.cyan },
+	},
+	update_in_insert = true,
+}
+local progress = function()
+	local current_line = vim.fn.line(".")
+	local total_lines = vim.fn.line("$")
+	local chars = { "__", "▁▁", "▂▂", "▃▃", "▄▄", "▅▅", "▆▆", "▇▇", "██" }
+	local line_ratio = current_line / total_lines
+	local index = math.ceil(line_ratio * #chars)
+	return " " .. chars[index] .. " "
+end
+
+local lsp_info = function()
 	if rawget(vim, "lsp") then
+		if next(vim.lsp.get_active_clients()) == nil then
+			return "No Active LSP "
+		end
 		for _, client in ipairs(vim.lsp.get_active_clients()) do
 			if client.attached_buffers[vim.api.nvim_get_current_buf()] then
-				return "%#St_LspStatus#" .. "   LSP ~ " .. client.name .. " "
+				return (vim.o.columns > 100 and "%#St_LspStatus#" .. "|   LSP ~ " .. client.name .. " ")
+					or "   LSP "
 			end
 		end
 	end
-end
-
-local mode = {
-	"mode",
-	fmt = function(str)
-		return " " .. str
-	end,
-}
-
-local function diagnostic()
-	if not rawget(vim, "lsp") then
-		return ""
-	end
-
-	local errors = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.ERROR })
-	local warnings = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.WARN })
-	local hints = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.HINT })
-	local info = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.INFO })
-
-	errors = (errors and errors > 0) and ("%#St_lspError#" .. " " .. errors .. " ") or ""
-	warnings = (warnings and warnings > 0) and ("%#St_lspWarning#" .. "  " .. warnings .. " ") or ""
-	hints = (hints and hints > 0) and ("%#St_lspHints#" .. "ﯧ " .. hints .. " ") or ""
-	info = (info and info > 0) and ("%#St_lspInfo#" .. " " .. info .. " ") or ""
-
-	return errors .. warnings .. hints .. info
-end
-
-local function git()
-	if not vim.b.gitsigns_head or vim.b.gitsigns_git_status then
-		return ""
-	end
-
-	local git_status = vim.b.gitsigns_status_dict
-
-	local added = (git_status.added and git_status.added ~= 0) and ("  " .. git_status.added) or ""
-	local changed = (git_status.changed and git_status.changed ~= 0) and ("  " .. git_status.changed) or ""
-	local removed = (git_status.removed and git_status.removed ~= 0) and ("  " .. git_status.removed) or ""
-	local branch_name = "  " .. git_status.head .. " "
-
-	return "%#St_gitIcons#" .. branch_name .. added .. changed .. removed
 end
 
 lualine.setup({
@@ -80,33 +115,26 @@ lualine.setup({
 		icons_enabled = true,
 		theme = "tokyonight",
 		component_separators = { left = "", right = "" },
-		section_separators = { left = "", right = "" },
-		disabled_filetypes = { "NvimTree", "toggleterm" },
+		section_separators = { left = "", right = "" },
+		disabled_filetypes = { "NvimTree" },
 		always_divide_middle = true,
 	},
 	sections = {
 		lualine_a = { mode },
-		lualine_b = { "filename" },
+		lualine_b = { file_info },
 		lualine_c = { git, "%=", lsp_progress },
-		lualine_x = { diagnostic, lsp },
-		lualine_y = {},
-		lualine_z = { "location" },
+		lualine_x = { diagnostic, lsp_info },
+		lualine_y = { location },
+		lualine_z = { progress },
 	},
 	inactive_sections = {
 		lualine_a = {},
 		lualine_b = {},
 		lualine_c = {},
-		lualine_x = {},
+		lualine_x = { "location" },
 		lualine_y = {},
 		lualine_z = {},
 	},
 	tabline = {},
 	extensions = {},
 })
-
-local hl = vim.api.nvim_set_hl
-hl(0, "St_lspError", { fg = "#e06c75", bg = "#1F2335" })
-hl(0, "St_lspWarning", { fg = "#FFFF00", bg = "#1F2335" })
-hl(0, "St_LspHints", { fg = "#00FFFF", bg = "#1F2335" })
-hl(0, "St_LspInfo", { fg = "#2EA043", bg = "#1F2335" })
-hl(0, "St_LspStatus", { fg = "#1CDEBB", bg = "#1F2335" })
