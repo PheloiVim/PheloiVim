848e9d849c85708a9fbea2c120f65677da549873
Vo Quang Chien <chiendien2006@gmail.com>
2023-02-26 08:32:59 +0700

    update options

--- lua/configs/options.lua
+++ lua/configs/options.lua
@@ -1,38 +1,100 @@
 local opt = vim.opt
-local g = vim.g
 
-opt.history = 0
-opt.shell = "fish"
+opt.autowrite = true -- Enable auto write
+opt.clipboard = "unnamedplus" -- Sync with system clipboard
+opt.backspace = { "nostop" }
+opt.cmdheight = 1
+opt.copyindent = true
+opt.completeopt = { "menu", "menuone", "noselect" }
+opt.conceallevel = 3 -- Hide * markup for bold and italic
+opt.confirm = true -- Confirm to save changes before exiting modified buffer
+opt.cursorline = true -- Enable highlighting of the current line
+opt.copyindent = true
+opt.fileencoding = "utf-8"
+opt.history = 100
+opt.preserveindent = true
+opt.expandtab = true -- Use spaces instead of tabs
+opt.formatoptions = "jcroqlnt" -- tcqj
+opt.grepformat = "%f:%l:%c:%m"
+opt.grepprg = "rg --vimgrep"
+opt.ignorecase = true -- Ignore case
+opt.inccommand = "nosplit" -- preview incremental substitute
 opt.laststatus = 0
-opt.hlsearch = true
-opt.backup = false
-opt.showmode = false
-opt.clipboard = "unnamedplus"
-opt.smartindent = true
-opt.cursorline = true
-opt.termguicolors = true
-opt.expandtab = true
-opt.wildmenu = false
-opt.completeopt = { "menuone", "noselect" }
-opt.shiftwidth = 4
-opt.tabstop = 4
-opt.wrap = true
-opt.softtabstop = 2
+opt.list = true -- Show some invisible characters (tabs...
 opt.fillchars = { eob = " " }
-opt.ignorecase = true
-opt.smartcase = true
-opt.mouse = "a"
-opt.number = true
+opt.mouse = "a" -- Enable mouse mode
+opt.number = true -- Print line number
+opt.pumblend = 10 -- Popup blend
+opt.pumheight = 10 -- Maximum number of entries in a popup
+opt.scrolloff = 4 -- Lines of context
+opt.sessionoptions = { "buffers", "curdir", "tabpages", "winsize" }
+opt.shiftround = true -- Round indent
+opt.shiftwidth = 2 -- Size of an indent
+opt.shortmess:append({ W = true, I = true, c = true })
+opt.showtabline = 2
+opt.showmode = false -- Dont show mode since we have a statusline
+opt.sidescrolloff = 8 -- Columns of context
+opt.signcolumn = "yes" -- Always show the signcolumn, otherwise it would shift the text each time
+opt.smartcase = true -- Don't ignore case with capitals
+opt.smartindent = true -- Insert indents automatically
 opt.numberwidth = 2
-opt.ruler = false
-opt.shortmess:append("sI")
-opt.signcolumn = "yes"
-opt.splitbelow = true
-opt.splitright = true
-opt.termguicolors = true
-opt.timeoutlen = 400
+opt.spelllang = { "en" }
+opt.splitbelow = true -- Put new windows below current
+opt.splitright = true -- Put new windows right of current
+opt.tabstop = 2 -- Number of spaces tabs count for
+opt.softtabstop = 2
+opt.termguicolors = true -- True color support
+opt.timeoutlen = 300 -- Length of time to wait for a mapped sequence
+opt.writebackup = false -- Disable making a backup before overwriting a file
 opt.undofile = true
-opt.updatetime = 250
-opt.swapfile = false
-g.loaded_netrw = 1
-g.loaded_netrwPlugin = 1
+opt.undolevels = 10000
+opt.updatetime = 200 -- Save swap file and trigger CursorHold
+opt.wrap = true -- Enable line wrap
+
+-- Fix markdown indentation settings
+vim.g.markdown_recommended_style = 0
+
+-- disable some builtin vim plugins
+local default_plugins = {
+	"2html_plugin",
+	"getscript",
+	"getscriptPlugin",
+	"gzip",
+	"logipat",
+	"netrw",
+	"netrwPlugin",
+	"netrwSettings",
+	"netrwFileHandlers",
+	"matchit",
+	"tar",
+	"tarPlugin",
+	"rrhelper",
+	"spellfile_plugin",
+	"vimball",
+	"vimballPlugin",
+	"zip",
+	"zipPlugin",
+	"tutor",
+	"rplugin",
+	"syntax",
+	"synmenu",
+	"optwin",
+	"compiler",
+	"bugreport",
+	"ftplugin",
+}
+
+for _, plugin in pairs(default_plugins) do
+	vim.g["loaded_" .. plugin] = 1
+end
+
+local default_providers = {
+	"node",
+	"perl",
+	"python3",
+	"ruby",
+}
+
+for _, provider in ipairs(default_providers) do
+	vim.g["loaded_" .. provider .. "_provider"] = 0
+end
