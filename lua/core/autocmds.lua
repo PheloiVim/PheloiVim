50725ce98055df77632149c301070a3733cedbfc
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-03-25 19:45:42 +0700

    update

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -1,10 +1,5 @@
-local function augroup(name)
-  return vim.api.nvim_create_augroup(name, { clear = true })
-end
-
 -- Check if we need to reload the file when it changed
 vim.api.nvim_create_autocmd({ "FocusGained", "TermClose", "TermLeave" }, {
-  group = augroup "checktime",
   callback = function()
     if vim.fn.getcmdwintype() == "" then
       vim.cmd "checktime"
@@ -14,14 +9,14 @@ vim.api.nvim_create_autocmd({ "FocusGained", "TermClose", "TermLeave" }, {
 
 -- Highlight on yank
 vim.api.nvim_create_autocmd("TextYankPost", {
-  group = augroup "highlight_yank",
+  pattern = "*",
   callback = function()
     vim.highlight.on_yank()
   end,
 })
 
 -- resize splits if window got resized
-vim.api.nvim_create_autocmd({ "VimResized" }, {
+vim.api.nvim_create_autocmd("VimResized", {
   callback = function()
     vim.cmd "tabdo wincmd ="
   end,
@@ -29,7 +24,6 @@ vim.api.nvim_create_autocmd({ "VimResized" }, {
 
 -- go to last loc when opening a buffer
 vim.api.nvim_create_autocmd("BufReadPost", {
-  group = augroup "last_loc",
   callback = function()
     local mark = vim.api.nvim_buf_get_mark(0, '"')
     local lcount = vim.api.nvim_buf_line_count(0)
@@ -41,7 +35,6 @@ vim.api.nvim_create_autocmd("BufReadPost", {
 
 -- close some filetypes with <q>
 vim.api.nvim_create_autocmd("FileType", {
-  group = augroup "close_with_q",
   pattern = {
     "qf",
     "help",
@@ -57,10 +50,23 @@ vim.api.nvim_create_autocmd("FileType", {
 
 -- wrap and check for spell in text filetypes
 vim.api.nvim_create_autocmd("FileType", {
-  group = augroup "wrap_spell",
   pattern = { "gitcommit", "markdown" },
   callback = function()
     vim.opt_local.wrap = true
     vim.opt_local.spell = true
+  end,
+})
+
+-- Auto create dir when saving a file, in case some intermediate directory does not exist
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  pattern = "*",
+  group = vim.api.nvim_create_augroup("auto_create_dir", { clear = true }),
+  callback = function(ctx)
+    local dir = vim.fn.fnamemodify(ctx.file, ":p:h")
+    local res = vim.fn.isdirectory(dir)
+
+    if res == 0 then
+      vim.fn.mkdir(dir, "p")
+    end
   end,
 })
