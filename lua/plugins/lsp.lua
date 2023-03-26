387e0b6751f158f712a62f07448b14fd06d4f377
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-03-26 11:00:11 +0700

    remove mason border

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -11,7 +11,7 @@ return {
       config = function()
         require("mason").setup {
           ui = {
-            border = "single",
+            border = "solid",
             icons = {
               package_installed = " ",
               package_pending = " ",
