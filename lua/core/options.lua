7e76769d24213a39df5f9183ba2c84adbd6ca297
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-04-02 18:53:35 +0700

    chore(options): completeopt

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -61,8 +61,6 @@ for _, provider in ipairs { "node", "perl", "python3", "ruby" } do
   vim.g["loaded_" .. provider .. "_provider"] = 0
 end
 
-vim.opt.completeopt = { "menu", "menuone" }
-
 -- Remove "How to disable mouse"
 vim.cmd [[
   aunmenu PopUp.How-to\ disable\ mouse
