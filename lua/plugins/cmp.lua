ced0064d3f3345b2e996007dac4227fd3076ee80
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-04-06 22:14:50 +0700

    feat(cmp): add cmdline completion

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -6,6 +6,7 @@ return {
       "L3MON4D3/LuaSnip",
       "rafamadriz/friendly-snippets",
       "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-cmdline",
       "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-path",
       "saadparwaiz1/cmp_luasnip",
@@ -92,6 +93,30 @@ return {
           },
         },
       }
+
+      cmp.setup.cmdline({ "/", "?" }, {
+        mapping = cmp.mapping.preset.cmdline(),
+        completion = { completeopt = "menu,menuone,noinsert,noselect" },
+        window = {
+          completion = { border = "solid" },
+        },
+        sources = {
+          { name = "buffer" },
+        },
+      })
+      cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline(),
+        formatting = { fields = { "abbr" } },
+        window = {
+          completion = { border = "solid" },
+        },
+        completion = { completeopt = "menu,menuone,noinsert,noselect" },
+        sources = cmp.config.sources({
+          { name = "path" },
+        }, {
+          { name = "cmdline" },
+        }),
+      })
     end,
   },
 }
