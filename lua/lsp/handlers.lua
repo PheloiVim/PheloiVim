1ca2ce2b1f253785de1d20d14e5da0113d12f81c
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-04-15 14:59:52 +0700

    feat(handler):remove numhl

--- lua/lsp/handlers.lua
+++ lua/lsp/handlers.lua
@@ -29,10 +29,11 @@ M.setup = function()
   }
 
   for _, sign in ipairs(signs) do
-    vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
+    vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text })
   end
 
   local config = {
+    enabled = true,
     virtual_text = {
       prefix = "",
     },
