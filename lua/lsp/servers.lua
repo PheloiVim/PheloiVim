e8c7578c41bc6fee4baa786c0abb1ea510152fc9
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-04-15 13:30:35 +0700

    fix(servers):remove mason lsp config

--- lua/lsp/servers.lua
+++ lua/lsp/servers.lua
@@ -11,25 +11,17 @@ local servers = {
   "rust_analyzer", -- Rust lsp
   "jsonls", -- Json lsp
 }
-local lspconfig = require "lspconfig"
 local opts = {}
-
-require("mason-lspconfig").setup {
-  ensure_installed = { "lua_ls", "html", "emmet_ls", "cssls", "tsserver", "rome", "bashls", "rust_analyzer", "jsonls" },
-}
 
 for _, server in pairs(servers) do
   opts = {
     on_attach = require("lsp.handlers").on_attach,
     capabilities = require("lsp.handlers").capabilities,
   }
-
   server = vim.split(server, "@")[1]
-
   local require_ok, conf_opts = pcall(require, "lsp.settings." .. server)
   if require_ok then
     opts = vim.tbl_deep_extend("force", conf_opts, opts)
   end
-
-  lspconfig[server].setup(opts)
+  require("lspconfig")[server].setup(opts)
 end
