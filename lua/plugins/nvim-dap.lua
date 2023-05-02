5e46ab8edf968ecf9d99fae6d5477144a1fe2529
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-05-02 13:21:36 +0700

    feat(nvim-dap):add dap

---
+++ lua/plugins/nvim-dap.lua
@@ -0,0 +1,95 @@
+return {
+  "mfussenegger/nvim-dap",
+  dependencies = {
+    "rcarriga/nvim-dap-ui",
+    opts = {
+      expand_lines = true,
+      icons = { expanded = "", collapsed = "", circular = "" },
+      mappings = {
+        expand = { "<CR>", "<2-LeftMouse>" },
+        open = "o",
+        remove = "d",
+        edit = "e",
+        repl = "r",
+        toggle = "t",
+      },
+      layouts = {
+        {
+          elements = {
+            { id = "scopes", size = 0.33 },
+            { id = "breakpoints", size = 0.17 },
+            { id = "stacks", size = 0.25 },
+            { id = "watches", size = 0.25 },
+          },
+          size = 0.33,
+          position = "right",
+        },
+        {
+          elements = {
+            { id = "repl", size = 0.45 },
+            { id = "console", size = 0.55 },
+          },
+          size = 0.27,
+          position = "bottom",
+        },
+      },
+      floating = {
+        max_height = 0.9,
+        max_width = 0.5,
+        border = "rounded",
+        mappings = {
+          close = { "q", "<Esc>" },
+        },
+      },
+    },
+  },
+  event = "VeryLazy",
+  config = function()
+    local dap = require "dap"
+    local dapui = require "dapui"
+    dap.listeners.after.event_initialized["dapui_config"] = function()
+      dapui.open()
+    end
+    dap.listeners.before.event_terminated["dapui_config"] = function()
+      dapui.close()
+    end
+    dap.listeners.before.event_exited["dapui_config"] = function()
+      dapui.close()
+    end
+    local sign = vim.fn.sign_define
+    sign("DapBreakpoint", { text = "●", texthl = "DapBreakpoint", linehl = "", numhl = "" })
+    sign("DapBreakpointCondition", { text = "●", texthl = "DapBreakpointCondition", linehl = "", numhl = "" })
+    sign("DapLogPoint", { text = "◆", texthl = "DapLogPoint", linehl = "", numhl = "" })
+
+    -- C/C++ debugger
+    dap.adapters.cppdbg = {
+      id = "cppdbg",
+      type = "executable",
+      command = "~/.local/share/nvim/mason/bin/OpenDebugAD7",
+    }
+    dap.configurations.cpp = {
+      {
+        name = "Launch file",
+        type = "cppdbg",
+        request = "launch",
+        program = function()
+          return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+        end,
+        cwd = "${workspaceFolder}",
+        stopAtEntry = true,
+      },
+      {
+        name = "Attach to gdbserver :1234",
+        type = "cppdbg",
+        request = "launch",
+        MIMode = "gdb",
+        miDebuggerServerAddress = "localhost:1234",
+        miDebuggerPath = "/usr/bin/gdb",
+        cwd = "${workspaceFolder}",
+        program = function()
+          return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+        end,
+      },
+    }
+  end,
+}
