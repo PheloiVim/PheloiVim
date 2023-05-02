11ecdc0348908b1de867a291838342ca5f3d3922
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-05-02 13:21:46 +0700

    feat(theme):enable native lsp

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -20,6 +20,25 @@ return {
         illuminate = true,
         lsp_trouble = true,
         notify = true,
+        native_lsp = {
+          enabled = true,
+          virtual_text = {
+            errors = { "italic" },
+            hints = { "italic" },
+            warnings = { "italic" },
+            information = { "italic" },
+          },
+          underlines = {
+            errors = { "underline" },
+            hints = { "underline" },
+            warnings = { "underline" },
+            information = { "underline" },
+          },
+        },
+        dap = {
+          enabled = true,
+          enable_ui = true,
+        },
       },
     }
     vim.cmd.colorscheme "catppuccin"
