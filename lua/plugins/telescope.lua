b8210efacaae280574e3f35b210ad5ed766e4d2f
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-05-09 15:23:15 +0700

    feat(telescope):telescope theme

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -9,7 +9,7 @@ return {
   },
   opts = function()
     local actions = require "telescope.actions"
-    local options = {
+    return {
       defaults = {
         vimgrep_arguments = {
           "rg",
@@ -30,18 +30,20 @@ return {
         layout_config = {
           horizontal = {
             prompt_position = "top",
-            preview_width = 0.55,
-            results_width = 0.8,
+            preview_width = 0.6,
+            results_width = 0.4,
           },
-          vertical = {
-            mirror = false,
-          },
-          width = 0.87,
-          height = 0.80,
+          width = 0.9,
+          height = 0.8,
           preview_cutoff = 120,
         },
+        pickers = {
+          find_files = {
+            theme = "dropdown",
+          },
+        },
         file_sorter = require("telescope.sorters").get_fuzzy_file,
-        file_ignore_patterns = { "node_modules" },
+        file_ignore_patterns = { ".git/", "node_modules" },
         generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
         path_display = { "truncate" },
         winblend = 0,
@@ -53,68 +55,39 @@ return {
         buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker,
         mappings = {
           i = {
-            ["<C-n>"] = actions.cycle_history_next,
-            ["<C-p>"] = actions.cycle_history_prev,
-
             ["<C-j>"] = actions.move_selection_next,
             ["<C-k>"] = actions.move_selection_previous,
-
             ["<C-c>"] = actions.close,
-
             ["<Down>"] = actions.move_selection_next,
             ["<Up>"] = actions.move_selection_previous,
-
             ["<CR>"] = actions.select_default,
-            ["<C-x>"] = actions.select_horizontal,
-            ["<C-v>"] = actions.select_vertical,
-            ["<C-t>"] = actions.select_tab,
-
-            ["<C-u>"] = actions.preview_scrolling_up,
-            ["<C-d>"] = actions.preview_scrolling_down,
-
-            ["<PageUp>"] = actions.results_scrolling_up,
-            ["<PageDown>"] = actions.results_scrolling_down,
-
-            ["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
-            ["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
-            ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
-            ["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-            ["<C-l>"] = actions.complete_tag,
-          },
-
-          n = {
-            ["<esc>"] = actions.close,
-            ["<CR>"] = actions.select_default,
-            ["<C-x>"] = actions.select_horizontal,
-            ["<C-v>"] = actions.select_vertical,
-            ["<C-t>"] = actions.select_tab,
-
-            ["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
-            ["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
-            ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
-            ["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-
-            ["j"] = actions.move_selection_next,
-            ["k"] = actions.move_selection_previous,
-            ["H"] = actions.move_to_top,
-            ["M"] = actions.move_to_middle,
-            ["L"] = actions.move_to_bottom,
-
-            ["<Down>"] = actions.move_selection_next,
-            ["<Up>"] = actions.move_selection_previous,
-            ["gg"] = actions.move_to_top,
-            ["G"] = actions.move_to_bottom,
-
-            ["<C-u>"] = actions.preview_scrolling_up,
-            ["<C-d>"] = actions.preview_scrolling_down,
-
-            ["<PageUp>"] = actions.results_scrolling_up,
-            ["<PageDown>"] = actions.results_scrolling_down,
           },
         },
       },
+      extensions = {
+        fzf = {
+          fuzzy = true,
+          override_generic_sorter = true,
+          override_file_sorter = true,
+          case_mode = "smart_case",
+        },
+      },
     }
+  end,
+  config = function(_, opts)
+    require("telescope").setup(opts)
     require("telescope").load_extension "fzf"
-    return options
+    local hl = vim.api.nvim_set_hl
+    local mainColor = "#0F1211"
+    hl(0, "TelescopeBorder", { fg = mainColor, bg = mainColor })
+    hl(0, "TelescopeNormal", { bg = mainColor })
+
+    hl(0, "TelescopePromptPrefix", { fg = "white", bg = mainColor })
+    hl(0, "TelescopePromptNormal", { fg = "white", bg = mainColor })
+    hl(0, "TelescopePromptBorder", { fg = mainColor, bg = mainColor })
+
+    hl(0, "TelescopePreviewTitle", { fg = "black", bg = "#00FFC1" })
+    hl(0, "TelescopeResultsTitle", { fg = "black", bg = "#FFEC00" })
+    hl(0, "TelescopePromptTitle", { fg = "black", bg = "#FF2D00" })
   end,
 }
