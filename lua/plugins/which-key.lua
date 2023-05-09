163fa7fc3fac2451f39e8aad3121151b729919bd
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-05-09 15:23:26 +0700

    feat(which-key):add which-key

---
+++ lua/plugins/which-key.lua
@@ -0,0 +1,68 @@
+return {
+  "folke/which-key.nvim",
+  keys = { "<leader>", '"', "'", "`", "c", "v", "d" },
+  opts = {
+    marks = true,
+    registers = true,
+    spelling = {
+      enabled = true,
+      suggestions = 20,
+    },
+    presets = {
+      operators = true,
+      motions = true,
+      text_objects = true,
+      windows = true,
+      nav = true,
+      z = true,
+      g = true,
+    },
+    motions = {
+      count = true,
+    },
+    icons = {
+      breadcrumb = "»",
+      separator = "➜",
+      group = "+",
+    },
+    window = {
+      border = "solid",
+      position = "bottom",
+      margin = { 1, 0, 1, 0 },
+      padding = { 2, 2, 2, 2 },
+      winblend = 0,
+    },
+    layout = {
+      height = { min = 4, max = 25 },
+      width = { min = 20, max = 50 },
+      spacing = 3,
+      align = "left",
+    },
+    key_labels = {
+      ["<space>"] = "SPC",
+    },
+    ignore_missing = true,
+    hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "^:", "^ ", "^call ", "^lua " },
+    show_help = false,
+    triggers = "auto",
+  },
+  config = function()
+    local mappings = {
+      l = {
+        name = "  LSP",
+        i = { "<cmd>LspInfo<cr>", "Info" },
+      },
+      f = {
+        name = "  Telescope",
+      },
+    }
+    require("which-key").register(mappings, {
+      mode = "n",
+      prefix = "<leader>",
+      buffer = nil,
+      silent = true,
+      noremap = true,
+      nowait = true,
+    })
+  end,
+}
