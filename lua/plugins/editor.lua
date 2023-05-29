d218fd32e9e9a0e5aa72240732e26806fc7e9a39
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-05-29 14:40:25 +0700

    update

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -458,10 +458,56 @@ return {
       show_current_context = true,
       buftype_exclude = { "terminal", "nofile" },
       filetype_exclude = {
+        "neo-tree",
         "help",
         "lazy",
         "alpha",
       },
+    },
+  },
+
+  {
+    "RRethy/vim-illuminate",
+    event = "VeryLazy",
+    opts = {
+      providers = {
+        "lsp",
+        "treesitter",
+        "regex",
+      },
+      delay = 200,
+      filetypes_denylist = {
+        "alpha",
+        "lazy",
+        "help",
+        "lspinfo",
+        "TelescopePrompt",
+        "toggleterm",
+      },
+    },
+    config = function(_, opts)
+      require("illuminate").configure(opts)
+      local function map(key, dir, buffer)
+        vim.keymap.set("n", key, function()
+          require("illuminate")["goto_" .. dir .. "_reference"](false)
+        end, { desc = dir:sub(1, 1):upper() .. dir:sub(2) .. " Reference", buffer = buffer })
+      end
+
+      map("]]", "next")
+      map("[[", "prev")
+
+      -- also set it after loading ftplugins, since a lot overwrite [[ and ]]
+      vim.api.nvim_create_autocmd("FileType", {
+        callback = function()
+          local buffer = vim.api.nvim_get_current_buf()
+          map("]]", "next", buffer)
+          map("[[", "prev", buffer)
+        end,
+      })
+    end,
+    keys = {
+      { "]]", desc = "Next Reference" },
+      { "[[", desc = "Prev Reference" },
     },
   },
 
@@ -502,6 +548,7 @@ return {
       },
       highlight = {
         enable = true,
+        use_languagetree = true,
         additional_vim_regex_highlighting = false,
       },
       indent = {
