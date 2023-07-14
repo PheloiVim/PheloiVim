55a96d2d79c904d43ea2cd2fd47b05e6eaba3756
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-07-14 21:55:49 +0700

    auto format for golang

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -62,3 +62,11 @@ vim.api.nvim_create_autocmd("BufWritePre", {
     vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
   end,
 })
+
+-- Auto format for golang
+local format_sync_grp = vim.api.nvim_create_augroup("GoImport", {})
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.go",
+  callback = function() require("go.format").goimport() end,
+  group = format_sync_grp,
+})
