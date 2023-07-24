a46e592aa52c20544c435a0ca9479aca66bd718b
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-07-24 15:58:39 +0700

    disable auto format

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,6 +1,7 @@
 vim.opt.whichwrap:append "<>[]hl"
 vim.opt.relativenumber = false
 vim.opt.numberwidth = 2
+vim.b.autoformat = false
 
 -- disable some default providers
 for _, provider in ipairs { "node", "perl", "python3", "ruby" } do
