aedd9952abe83230aa991143c9be53366276da6f
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-09-02 15:01:07 +0700

    fix

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,17 +1,26 @@
 return {
-  "hrsh7th/nvim-cmp",
-  opts = function(_, opts)
-    require("luasnip/loaders/from_vscode").lazy_load()
-    opts.window = {
-      completion = { border = "solid" },
-      documentation = { border = "solid" },
-    }
-    opts.mapping = require("cmp").mapping.preset.insert {
-      ["<C-j>"] = require("cmp").mapping.select_next_item { behavior = require("cmp").SelectBehavior.Insert },
-      ["<C-k>"] = require("cmp").mapping.select_prev_item { behavior = require("cmp").SelectBehavior.Insert },
-      ["<C-u>"] = require("cmp").mapping.scroll_docs(-4),
-      ["<C-d>"] = require("cmp").mapping.scroll_docs(4),
-      ["<TAB>"] = require("cmp").mapping.confirm { select = true },
-    }
-  end,
+  {
+    "L3MON4D3/LuaSnip",
+    opts = {
+      region_check_events = "InsertEnter",
+      delete_check_events = "TextChanged,InsertLeave",
+    },
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      require("luasnip/loaders/from_vscode").lazy_load()
+      opts.window = {
+        completion = { border = "solid" },
+        documentation = { border = "solid" },
+      }
+      opts.mapping = require("cmp").mapping.preset.insert {
+        ["<C-j>"] = require("cmp").mapping.select_next_item { behavior = require("cmp").SelectBehavior.Insert },
+        ["<C-k>"] = require("cmp").mapping.select_prev_item { behavior = require("cmp").SelectBehavior.Insert },
+        ["<C-u>"] = require("cmp").mapping.scroll_docs(-4),
+        ["<C-d>"] = require("cmp").mapping.scroll_docs(4),
+        ["<TAB>"] = require("cmp").mapping.confirm { select = true },
+      }
+    end,
+  },
 }
