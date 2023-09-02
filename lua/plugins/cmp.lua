47b9b06e10001032e93fc464c3d18b2a2df2d0b2
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-09-02 08:39:53 +0700

    update

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,50 +1,20 @@
 return {
   "hrsh7th/nvim-cmp",
-  opts = function()
-    vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
-    local cmp = require "cmp"
-    local defaults = require "cmp.config.default"()
-    require("luasnip/loaders/from_vscode").lazy_load()
-
-    return {
-      completion = { completeopt = "menu,menuone,noinsert" },
-      snippet = {
-        expand = function(args) require("luasnip").lsp_expand(args.body) end,
-      },
-      window = {
-        completion = { border = "solid" },
-        documentation = { border = "solid" },
-      },
-      formatting = {
-        format = function(_, item)
-          local icons = require("lazyvim.config").icons.kinds
-          if icons[item.kind] then item.kind = icons[item.kind] .. item.kind end
-          return item
-        end,
-      },
-      sources = cmp.config.sources {
-        { name = "nvim_lsp" },
-        { name = "luasnip" },
-        { name = "buffer" },
-        { name = "path" },
-      },
-      mapping = {
-        ["<Up>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Select },
-        ["<Down>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Select },
-        ["<C-k>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
-        ["<C-j>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
-        ["<C-u>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
-        ["<C-d>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
-        ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-        ["<C-e>"] = cmp.mapping { i = cmp.mapping.abort(), c = cmp.mapping.close() },
-        ["<TAB>"] = cmp.mapping.confirm { select = false },
-      },
-      experimental = {
-        ghost_text = {
-          hl_group = "CmpGhostText",
-        },
-      },
-      sorting = defaults.sorting,
-    }
-  end,
+  opts = {
+    window = {
+      completion = { border = "solid" },
+      documentation = { border = "solid" },
+    },
+    mapping = {
+      ["<Up>"] = require("cmp").mapping.select_prev_item { behavior = require("cmp").SelectBehavior.Select },
+      ["<Down>"] = require("cmp").mapping.select_next_item { behavior = require("cmp").SelectBehavior.Select },
+      ["<C-k>"] = require("cmp").mapping.select_prev_item { behavior = require("cmp").SelectBehavior.Insert },
+      ["<C-j>"] = require("cmp").mapping.select_next_item { behavior = require("cmp").SelectBehavior.Insert },
+      ["<C-u>"] = require("cmp").mapping(require("cmp").mapping.scroll_docs(-4), { "i", "c" }),
+      ["<C-d>"] = require("cmp").mapping(require("cmp").mapping.scroll_docs(4), { "i", "c" }),
+      ["<C-Space>"] = require("cmp").mapping(require("cmp").mapping.complete(), { "i", "c" }),
+      ["<C-e>"] = require("cmp").mapping { i = require("cmp").mapping.abort(), c = require("cmp").mapping.close() },
+      ["<TAB>"] = require("cmp").mapping.confirm { select = false },
+    },
+  },
 }
