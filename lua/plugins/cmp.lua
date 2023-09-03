24ce951d5d164f5fe477dfbc0ce8ee1e926c1fcc
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-09-03 20:24:16 +0700

    update

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -9,7 +9,7 @@ return {
   {
     "hrsh7th/nvim-cmp",
     opts = function(_, opts)
-      require("luasnip/loaders/from_vscode").lazy_load()
+      require("luasnip.loaders.from_snipmate").lazy_load()
       local cmp = require "cmp"
       opts.window = {
         completion = { border = "solid" },
