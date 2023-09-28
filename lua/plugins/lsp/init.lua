9966c9becc480a5aff715f6ab7edde483e82b730
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-09-28 21:34:32 +0700

    update

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -5,7 +5,73 @@ return {
     dependencies = {
       "mason.nvim",
       "mason-lspconfig.nvim",
+      { "folke/neodev.nvim", opts = {} },
     },
+    opts = {
+      diagnostics = {
+        underline = true,
+        update_in_insert = true,
+        signs = true,
+        virtual_text = {
+          spacing = 4,
+          source = "if_many",
+          prefix = "●",
+        },
+        severity_sort = true,
+      },
+      servers = {
+        lua_ls = {
+          settings = {
+            Lua = {
+              diagnostics = {
+                globals = { "vim" },
+              },
+              workspace = {
+                maxPreload = 50000,
+                preloadFileSize = 10000,
+                checkThirdParty = false,
+              },
+              completion = {
+                callSnippet = "Replace",
+              },
+            },
+            hint = { enable = true },
+          },
+        },
+      },
+    },
+    config = function(_, opts)
+      local capabilities = vim.tbl_deep_extend("force", vim.lsp.protocol.make_client_capabilities(), require("cmp_nvim_lsp").default_capabilities())
+
+      vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+        border = "rounded",
+      })
+      vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+        border = "rounded",
+      })
+
+      for name, icon in pairs(require("icons").diagnostics) do
+        name = "DiagnosticSign" .. name
+        vim.fn.sign_define(name, { text = icon, texthl = name, numhl = "" })
+      end
+      vim.diagnostic.config(vim.deepcopy(opts.diagnostics))
+
+      local servers = opts.servers
+
+      local function setup(server)
+        local server_opts = vim.tbl_deep_extend("force", {
+          capabilities = vim.deepcopy(capabilities),
+        }, servers[server] or {})
+        require("lspconfig")[server].setup(server_opts)
+      end
+
+      for server, server_opts in pairs(servers) do
+        if server_opts then
+          server_opts = server_opts == true and {} or server_opts
+          setup(server)
+        end
+      end
+    end,
   },
 
   {
