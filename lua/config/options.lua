eb5b35fc0e8232b27b6fb242680a17d99d8eec8b
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-09-30 22:48:15 +0700

    add shell config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,7 @@ vim.opt.numberwidth = 2
 vim.opt.writebackup = false
 vim.opt.backup = false
 vim.opt.virtualedit = "block"
+vim.opt.shell = "fish"
 
 -- disable some default providers
 for _, provider in ipairs({ "node", "perl", "python3", "ruby" }) do
