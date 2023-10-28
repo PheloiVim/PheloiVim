2d0818812cc9f3345f90d1e8ccba0cb8c31ae5a5
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-10-28 23:11:00 +0700

    Lien Chi

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -132,6 +132,41 @@ return {
   },
 
   {
+    "L3MON4D3/LuaSnip",
+    keys = function()
+      return {}
+    end,
+  },
+
+  {
+    "nvim-cmp",
+    opts = function(_, opts)
+      local has_words_before = function()
+        unpack = unpack or table.unpack
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+      end
+
+      local luasnip = require("luasnip")
+      local cmp = require("cmp")
+
+      opts.mapping = vim.tbl_extend("force", opts.mapping, {
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.confirm()
+          elseif luasnip.expand_or_locally_jumpable() then
+            luasnip.expand_or_jump()
+          elseif has_words_before() then
+            cmp.complete()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+      })
+    end,
+  },
+
+  {
     "nvim-treesitter",
     opts = function(_, opts)
       vim.list_extend(opts.ensure_installed, {
