3647963b531a46610b36011087f59875b333c780
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-11-20 21:36:20 +0700

    add neogit

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -5,4 +5,18 @@ return {
     dependencies = { "nvim-tree/nvim-web-devicons" },
     opts = {},
   },
+
+  {
+    "NeogitOrg/neogit",
+    cmd = "Neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+      "sindrets/diffview.nvim",
+      "ibhagwan/fzf-lua",
+    },
+    opts = {
+      disable_hint = true,
+    },
+  },
 }
