363770a9d6ebf3944ba2004172f36c6ef778fb7a
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-12-18 22:13:38 +0700

    change nixpkgs to unstable channel

--- flake.nix
+++ flake.nix
@@ -1,13 +1,19 @@
 {
-  description = "Setup packages for neovim-config";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+  description = "Neovim config based on LazyVim";
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+  };
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system}; in {
         devShell = pkgs.mkShell {
-          buildInputs = [
-            pkgs.stylua
+          buildInputs = with pkgs; [
+            stylua
+            nodejs_21
+            rustc
+            cargo
+            clippy
           ];
         };
         formatter = pkgs.nixpkgs-fmt;
