cafaaa48b2733be66a23746f0da00040ea51a2c3
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-12-31 20:55:55 +0700

    updates

--- lua/pheloivim/plugins/telescope.lua
+++ lua/pheloivim/plugins/telescope.lua
@@ -11,6 +11,44 @@ return {
       end,
     },
   },
-  keys = {},
-  opts = {},
+  keys = {
+    { "<leader>ft", "<cmd>Telescope live_grep<cr>", desc = "Find text" },
+    { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
+    { "<leader>fr", "<cmd>Telescope oldfiles<cr>", desc = "Recent" },
+  },
+  opts = {
+    theme = "dropdown",
+    defaults = {
+      prompt_prefix = " ",
+      selection_caret = " ",
+      path_display = { "smart" },
+      layout_config = {
+        prompt_position = "top",
+        horizontal = {
+          preview_cutoff = 80,
+          width = 0.9,
+        },
+      },
+      get_selection_window = function()
+        local wins = vim.api.nvim_list_wins()
+        table.insert(wins, 1, vim.api.nvim_get_current_win())
+        for _, win in ipairs(wins) do
+          local buf = vim.api.nvim_win_get_buf(win)
+          if vim.bo[buf].buftype == "" then
+            return win
+          end
+        end
+        return 0
+      end,
+      sorting_strategy = "ascending",
+    },
+    extensions = {
+      fzf = {
+        fuzzy = true,
+        override_generic_sorter = true,
+        override_file_sorter = true,
+        case_mode = "smart_case",
+      },
+    },
+  },
 }
