bf090509d759b5c584927c1b651fdbc33f84031e
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-01-01 20:27:21 +0700

    feat(bash): better bash highlighting

--- lua/pheloivim/pack/bash.lua
+++ lua/pheloivim/pack/bash.lua
@@ -1,7 +1,7 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts) table.insert(opts.ensure_installed, "bash") end,
+    opts = function(_, opts) vim.list_extend(opts.ensure_installed, { "bash" }) end,
   },
 
   {
