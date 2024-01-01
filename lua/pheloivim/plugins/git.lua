543757fc92e88ea9a5f3c19b03bf9e4eb4efdd6f
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-01-01 21:03:27 +0700

    refactor(git): move all git plugins to `git.lua`

---
+++ lua/pheloivim/plugins/git.lua
@@ -0,0 +1,52 @@
+return {
+  {
+    "lewis6991/gitsigns.nvim",
+    event = { "BufReadPost", "BufNewFile", "BufWritePost" },
+    opts = {
+      signs = {
+        add = { text = "│" },
+        change = { text = "│" },
+        delete = { text = "_" },
+        topdelete = { text = "‾" },
+        changedelete = { text = "~" },
+        untracked = { text = "▎" },
+      },
+      preview_config = {
+        border = "rounded",
+        style = "minimal",
+        relative = "cursor",
+      },
+      on_attach = function(bufnr)
+        local gs = require("gitsigns")
+
+        local function map(mode, l, r, desc) vim.keymap.set(mode, l, r, { buffer = bufnr, desc = desc }) end
+
+        map("n", "]h", gs.next_hunk, "Next Hunk")
+        map("n", "[h", gs.prev_hunk, "Prev Hunk")
+        map({ "n", "v" }, "<leader>ghs", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
+        map({ "n", "v" }, "<leader>ghr", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
+        map("n", "<leader>ghS", gs.stage_buffer, "Stage Buffer")
+        map("n", "<leader>ghu", gs.undo_stage_hunk, "Undo Stage Hunk")
+        map("n", "<leader>ghR", gs.reset_buffer, "Reset Buffer")
+        map("n", "<leader>ghp", gs.preview_hunk, "Preview Hunk")
+        map("n", "<leader>ghd", gs.diffthis, "Diff This")
+      end,
+    },
+  },
+
+  {
+    "NeogitOrg/neogit",
+    cmd = "Neogit",
+    ft = { "diff" },
+    keys = {
+      { "<leader>gnt", "<cmd>Neogit<cr>", desc = "Open Neogit Tab Page" },
+      { "<leader>gnc", "<cmd>Neogit commit<cr>", desc = "Open Neogit Commit Page" },
+    },
+    dependencies = {
+      { "nvim-lua/plenary.nvim" },
+    },
+    opts = {
+      disable_hint = true,
+    },
+  },
+}
