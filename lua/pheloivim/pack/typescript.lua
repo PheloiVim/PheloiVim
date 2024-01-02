a7a0af25fb2530fa201b64251a3a7dbd174a5dd0
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-01-02 23:41:09 +0700

    feat: add support for typescript

--- lua/pheloivim/pack/typescript.lua
+++ lua/pheloivim/pack/typescript.lua
@@ -1,2 +1,91 @@
--- TODO: add support for typescript
-return {}
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts) vim.list_extend(opts.ensure_installed, { "typescript", "tsx", "javascript", "jsdoc" }) end,
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        ---@type lspconfig.options.tsserver
+        tsserver = {
+          settings = {
+            ---@diagnostic disable-next-line
+            typescript = {
+              ---@diagnostic disable-next-line
+              format = {
+                indentSize = vim.o.shiftwidth,
+                convertTabsToSpaces = vim.o.expandtab,
+                tabSize = vim.o.tabstop,
+              },
+            },
+            ---@diagnostic disable-next-line
+            javascript = {
+              ---@diagnostic disable-next-line
+              format = {
+                indentSize = vim.o.shiftwidth,
+                convertTabsToSpaces = vim.o.expandtab,
+                tabSize = vim.o.tabstop,
+              },
+            },
+            completions = {
+              completeFunctionCalls = true,
+            },
+          },
+        },
+      },
+    },
+  },
+
+  {
+    "mfussenegger/nvim-dap",
+    optional = true,
+    opts = function()
+      require("pheloivim.utils").install_dap("js-debug-adapter")
+      local dap = require("dap")
+      if not dap.adapters["pwa-node"] then
+        require("dap").adapters["pwa-node"] = {
+          type = "server",
+          host = "localhost",
+          port = "${port}",
+          executable = {
+            command = "node",
+            args = {
+              require("mason-registry").get_package("js-debug-adapter"):get_install_path()
+                .. "/js-debug/src/dapDebugServer.js",
+              "${port}",
+            },
+          },
+        }
+      end
+      for _, language in ipairs({ "typescript", "javascript", "typescriptreact", "javascriptreact" }) do
+        if not dap.configurations[language] then
+          dap.configurations[language] = {
+            {
+              type = "pwa-node",
+              request = "launch",
+              name = "Launch file",
+              program = "${file}",
+              cwd = "${workspaceFolder}",
+            },
+            {
+              type = "pwa-node",
+              request = "attach",
+              name = "Attach",
+              processId = require("dap.utils").pick_process,
+              cwd = "${workspaceFolder}",
+            },
+          }
+        end
+      end
+    end,
+  },
+
+  {
+    "vuki656/package-info.nvim",
+    dependencies = { "MunifTanjim/nui.nvim" },
+    opts = {},
+    event = "BufRead package.json",
+  },
+}
