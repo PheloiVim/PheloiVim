9d130355612815865ecda1d132428676a373f51d
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-01-02 16:20:01 +0700

    feat!: install fmt and linter manually

--- lua/pheloivim/plugins/conform.lua
+++ lua/pheloivim/plugins/conform.lua
@@ -11,13 +11,4 @@ return {
   opts = {
     formatters_by_ft = {},
   },
-  config = function(_, opts)
-    for _, list_formatters in pairs(opts.formatters_by_ft) do
-      for _, fmt in ipairs(list_formatters) do
-        require("pheloivim.utils").install_package(fmt)
-      end
-    end
-
-    require("conform").setup(opts)
-  end,
 }
