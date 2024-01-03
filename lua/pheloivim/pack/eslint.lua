a95af4f9b93418393d622afa025f1a1bf7a323cc
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-01-03 12:40:14 +0700

    feat: add eslint

--- lua/pheloivim/pack/eslint.lua
+++ lua/pheloivim/pack/eslint.lua
@@ -1,2 +1,17 @@
--- TODO: add support for eslint
-return {}
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      ---@type lspconfig.options
+      ---@diagnostic disable-next-line
+      servers = {
+        eslint = {
+          ---@diagnostic disable-next-line
+          settings = {
+            workingDirectory = { mode = "auto" },
+          },
+        },
+      },
+    },
+  },
+}
