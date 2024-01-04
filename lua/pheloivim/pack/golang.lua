e9048266a2959cd2ef7399291557670fc9819957
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-01-04 15:30:38 +0700

    feat(go): use `go.nvim` gopls config

--- lua/pheloivim/pack/golang.lua
+++ lua/pheloivim/pack/golang.lua
@@ -13,48 +13,20 @@ return {
 
   {
     "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        gopls = {
-          settings = {
-            gopls = {
-              gofumpt = true,
-              codelenses = {
-                gc_details = false,
-                generate = true,
-                regenerate_cgo = true,
-                run_govulncheck = true,
-                test = true,
-                tidy = true,
-                upgrade_dependency = true,
-                vendor = true,
-              },
-              hints = {
-                assignVariableTypes = true,
-                compositeLiteralFields = true,
-                compositeLiteralTypes = true,
-                constantValues = true,
-                functionTypeParameters = true,
-                parameterNames = true,
-                rangeVariableTypes = true,
-              },
-              analyses = {
-                fieldalignment = true,
-                nilness = true,
-                unusedparams = true,
-                unusedwrite = true,
-                useany = true,
-              },
-              usePlaceholders = true,
-              completeUnimported = true,
-              staticcheck = true,
-              directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
-              semanticTokens = true,
-            },
-          },
-        },
+    dependencies = {
+      "ray-x/guihua.lua",
+      {
+        "ray-x/go.nvim",
+        event = { "BufReadPost", "BufNewFile", "BufWritePost" },
+        ft = { "go", "gomod" },
+        opts = { lsp_cfg = false },
       },
     },
+    opts = function(_, opts)
+      opts.servers = vim.tbl_deep_extend("force", opts.servers, {
+        gopls = require("go.lsp").config(),
+      })
+    end,
   },
 
   {
@@ -68,18 +40,6 @@ return {
         go = { "goimports", "gofumpt" },
       })
     end,
-  },
-
-  {
-    "ray-x/go.nvim",
-    dependencies = {
-      "ray-x/guihua.lua",
-      "neovim/nvim-lspconfig",
-      "nvim-treesitter/nvim-treesitter",
-    },
-    event = { "BufReadPost", "BufNewFile", "BufWritePost" },
-    ft = { "go", "gomod" },
-    opts = {},
   },
 
   {
