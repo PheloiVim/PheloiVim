8c2e2f68ddbcb4e81b346e618d5acdcf73f742d3
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-01-04 15:00:23 +0700

    feat: add golang support

--- lua/pheloivim/pack/golang.lua
+++ lua/pheloivim/pack/golang.lua
@@ -1,2 +1,90 @@
--- TODO: add support for golang
-return {}
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "go",
+        "gomod",
+        "gowork",
+        "gosum",
+      })
+    end,
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        gopls = {
+          settings = {
+            gopls = {
+              gofumpt = true,
+              codelenses = {
+                gc_details = false,
+                generate = true,
+                regenerate_cgo = true,
+                run_govulncheck = true,
+                test = true,
+                tidy = true,
+                upgrade_dependency = true,
+                vendor = true,
+              },
+              hints = {
+                assignVariableTypes = true,
+                compositeLiteralFields = true,
+                compositeLiteralTypes = true,
+                constantValues = true,
+                functionTypeParameters = true,
+                parameterNames = true,
+                rangeVariableTypes = true,
+              },
+              analyses = {
+                fieldalignment = true,
+                nilness = true,
+                unusedparams = true,
+                unusedwrite = true,
+                useany = true,
+              },
+              usePlaceholders = true,
+              completeUnimported = true,
+              staticcheck = true,
+              directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
+              semanticTokens = true,
+            },
+          },
+        },
+      },
+    },
+  },
+
+  {
+    "stevearc/conform.nvim",
+    opts = function(_, opts)
+      require("pheloivim.utils").install_formatter("goimports")
+      require("pheloivim.utils").install_formatter("gofumpt")
+
+      opts.formatters_by_ft = vim.tbl_deep_extend("force", opts.formatters_by_ft, {
+        go = { "goimports", "gofumpt" },
+      })
+    end,
+  },
+
+  {
+    "ray-x/go.nvim",
+    dependencies = {
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    event = { "BufReadPost", "BufNewFile", "BufWritePost" },
+    ft = { "go", "gomod" },
+    opts = {},
+  },
+
+  {
+    "mfussenegger/nvim-dap",
+    optional = true,
+    dependencies = { "leoluz/nvim-dap-go", opts = {} },
+    opts = function() require("pheloivim.utils").install_dap("delve") end,
+  },
+}
