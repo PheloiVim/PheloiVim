81588f00d489eb71303f622888471cfedc3c9ea6
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-01-08 13:06:48 +0700

    feat(docker): add hadolint

--- lua/pheloivim/pack/docker.lua
+++ lua/pheloivim/pack/docker.lua
@@ -1,4 +1,3 @@
--- TODO: add support for docker
 return {
   {
     "nvim-treesitter/nvim-treesitter",
@@ -13,5 +12,15 @@ return {
         docker_compose_language_service = {},
       },
     },
+  },
+
+  {
+    "mfussenegger/nvim-lint",
+    opts = function(_, opts)
+      require("pheloivim.utils").install_linter("hadolint")
+      opts.linters_by_ft = vim.tbl_deep_extend("force", opts.linters_by_ft, {
+        dockerfile = { "hadolint" },
+      })
+    end,
   },
 }
