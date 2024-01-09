2ab4f0dfbd6e873b7341b34cbea968b2941b7a5b
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-01-09 12:46:49 +0700

    feat(lang): add csharp support

--- lua/pheloivim/pack/csharp.lua
+++ lua/pheloivim/pack/csharp.lua
@@ -1,2 +1,39 @@
--- TODO: setup omnisharp, csharpier, dap, neotest
-return {}
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts) vim.list_extend(opts.ensure_installed, { "c_sharp" }) end,
+  },
+
+  {
+    "stevearc/conform.nvim",
+    opts = function(_, opts)
+      require("pheloivim.utils").install_formatter("csharpier")
+
+      opts.formatters_by_ft = {
+        cs = { "csharpier" },
+      }
+    end,
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = "Hoffs/omnisharp-extended-lsp.nvim",
+    opts = {
+      servers = {
+        omnisharp = {
+          handlers = {
+            ["textDocument/definition"] = function(...) return require("omnisharp_extended").handler(...) end,
+          },
+          keys = {
+            n = {
+              ["gd"] = { cmd = function() require("omnisharp_extended").telescope_lsp_definitions() end, desc = "Goto Definition" },
+            },
+          },
+          enable_roslyn_analyzers = true,
+          organize_imports_on_format = true,
+          enable_import_completion = true,
+        },
+      },
+    },
+  },
+}
