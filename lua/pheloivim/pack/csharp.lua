fe850bcaddd326c3b33f4fe91738734d5690829b
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-01-09 12:50:52 +0700

    chore: format code

--- lua/pheloivim/pack/csharp.lua
+++ lua/pheloivim/pack/csharp.lua
@@ -36,4 +36,10 @@ return {
       },
     },
   },
+
+  {
+    "nvim-neotest/neotest",
+    dependencies = "Issafalcon/neotest-dotnet",
+    opts = function(_, opts) vim.list_extend(opts.adapters, { require("neotest-dotnet") }) end,
+  },
 }
--- lua/pheloivim/pack/golang.lua
+++ lua/pheloivim/pack/golang.lua
@@ -51,9 +51,7 @@ return {
 
   {
     "nvim-neotest/neotest",
-    dependencies = {
-      "nvim-neotest/neotest-go",
-    },
+    dependencies = "nvim-neotest/neotest-go",
     opts = function(_, opts) vim.list_extend(opts.adapters, { require("neotest-go") }) end,
   },
 
