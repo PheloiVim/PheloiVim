2afbaccc464e6930f5dd99846ae8efcd11299a16
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-01-11 15:19:03 +0700

    feat(terraform): add format, lint

--- lua/pheloivim/pack/terraform.lua
+++ lua/pheloivim/pack/terraform.lua
@@ -13,4 +13,25 @@ return {
       },
     },
   },
+
+  {
+    "mfussenegger/nvim-lint",
+    opts = {
+      linters_by_ft = {
+        terraform = { "terraform_validate" },
+        tf = { "terraform_validate" },
+      },
+    },
+  },
+
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        terraform = { "terraform_fmt" },
+        tf = { "terraform_fmt" },
+        ["terraform-vars"] = { "terraform_fmt" },
+      },
+    },
+  },
 }
