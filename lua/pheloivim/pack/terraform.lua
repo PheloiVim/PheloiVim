b535707b751c0ce04443b0ee6a8df485305d494b
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-01-11 15:11:23 +0700

    feat: add terraform support

--- lua/pheloivim/pack/terraform.lua
+++ lua/pheloivim/pack/terraform.lua
@@ -1,2 +1,16 @@
--- TODO: support terraform, hcl
-return {}
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts) opts.ensure_installed = vim.list_extend(opts.ensure_installed, { "terraform", "hcl" }) end,
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        terraformls = {},
+        tflint = {},
+      },
+    },
+  },
+}
