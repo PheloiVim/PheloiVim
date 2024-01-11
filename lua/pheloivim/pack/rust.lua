7e64e1b274f128ed161acf8b8fa2b0d58f46b8a4
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-01-11 15:19:40 +0700

    refactor(rust): use opts table

--- lua/pheloivim/pack/rust.lua
+++ lua/pheloivim/pack/rust.lua
@@ -35,11 +35,11 @@ return {
 
   {
     "stevearc/conform.nvim",
-    opts = function(_, opts)
-      opts.formatters_by_ft = vim.tbl_deep_extend("force", opts.formatters_by_ft, {
+    opts = {
+      formatters_by_ft = {
         rust = { "rustfmt" },
-      })
-    end,
+      },
+    },
   },
 
   {
