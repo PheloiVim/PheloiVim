4f117899e8bddd2a8ea1f52e502c8e47fa3f636e
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-01-23 15:44:00 +0700

    feat(colorscheme): tokyonight telescope borderless

--- lua/pheloivim/plugins/colorscheme.lua
+++ lua/pheloivim/plugins/colorscheme.lua
@@ -14,6 +14,35 @@ return {
     opts = {
       ---@type "storm" | "moon" | "night" | "day"
       style = "moon",
+      on_highlights = function(hl, c)
+        hl.TelescopeNormal = {
+          bg = c.bg_dark,
+          fg = c.fg_dark,
+        }
+        hl.TelescopeBorder = {
+          bg = c.bg_dark,
+          fg = c.bg_dark,
+        }
+        hl.TelescopePromptNormal = {
+          bg = "#2d3149",
+        }
+        hl.TelescopePromptBorder = {
+          bg = "#2d3149",
+          fg = "#2d3149",
+        }
+        hl.TelescopePromptTitle = {
+          fg = "#2d3149",
+          bg = "#2d3149",
+        }
+        hl.TelescopePreviewTitle = {
+          bg = c.bg_dark,
+          fg = c.bg_dark,
+        }
+        hl.TelescopeResultsTitle = {
+          bg = c.bg_dark,
+          fg = c.bg_dark,
+        }
+      end,
     },
   },
 
