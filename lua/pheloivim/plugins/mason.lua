56acb746674fc79810253e16c8210d06ec160001
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-01-24 05:31:40 +0700

    fix(mason): use `vim.schedule_wrap`

--- lua/pheloivim/plugins/mason.lua
+++ lua/pheloivim/plugins/mason.lua
@@ -34,7 +34,7 @@ return {
             vim.notify(("[pheloivim-mason] installing %s"):format(pkg.name), vim.log.levels.INFO)
             pkg:install():once(
               "closed",
-              vim.schedule(function()
+              vim.schedule_wrap(function()
                 if pkg:is_installed() then vim.notify(("[pheloivim-mason] %s was installed"):format(pkg.name), vim.log.levels.INFO) end
               end)
             )
