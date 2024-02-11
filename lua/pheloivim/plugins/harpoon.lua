1a3aa697dfa48a4eac0e95432f2ca46e9ca35af9
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-02-11 15:55:02 +0700

    feat(harpoon)!: migrate to harpoon2

--- lua/pheloivim/plugins/harpoon.lua
+++ lua/pheloivim/plugins/harpoon.lua
@@ -1,12 +1,36 @@
+local function toggle_telescope(harpoon_files)
+  local conf = require("telescope.config").values
+  local file_paths = {}
+  for _, item in ipairs(harpoon_files.items) do
+    table.insert(file_paths, item.value)
+  end
+
+  require("telescope.pickers")
+    .new({}, {
+      prompt_title = "Harpoon",
+      finder = require("telescope.finders").new_table({
+        results = file_paths,
+      }),
+      previewer = conf.file_previewer({}),
+      sorter = conf.generic_sorter({}),
+    })
+    :find()
+end
+
 return {
   "ThePrimeagen/harpoon",
+  branch = "harpoon2",
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-telescope/telescope.nvim",
   },
-  keys = {
-    { "<leader>fa", function() require("harpoon.mark").add_file() end, desc = "Add file" },
-    { "<leader>fe", function() require("harpoon.ui").toggle_quick_menu() end, desc = "Toggle quick menu" },
-    { "<leader>fm", "<cmd>Telescope harpoon marks<CR>", desc = "Show marks in Telescope" },
-  },
+  keys = function()
+    local harpoon = require("harpoon")
+    return {
+      { "<leader>fa", function() harpoon:list():append() end, desc = "Add file" },
+      { "<leader>fe", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, desc = "Toggle quick menu" },
+      { "<leader>fm", function() toggle_telescope(harpoon:list()) end, desc = "Show marks in Telescope" },
+    }
+  end,
+  opts = {},
 }
