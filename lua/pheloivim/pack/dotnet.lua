1a9caf6288fb2a9b79bddb4c49c00341c6b86ca4
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-02-11 23:08:51 +0700

    update :v

--- lua/pheloivim/pack/dotnet.lua
+++ lua/pheloivim/pack/dotnet.lua
@@ -33,6 +33,7 @@ return {
               ["gd"] = { cmd = function() require("omnisharp_extended").telescope_lsp_definitions() end, desc = "Goto Definition" },
             },
           },
+          enable_ms_build_load_projects_on_demand = true,
           enable_roslyn_analyzers = true,
           organize_imports_on_format = true,
           enable_import_completion = true,
