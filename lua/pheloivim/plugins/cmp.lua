d9733bc5ccb1f08e4e74ae7736ca2f3e24b28c7d
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-02-12 15:49:25 +0700

    update cmp keymap

--- lua/pheloivim/plugins/cmp.lua
+++ lua/pheloivim/plugins/cmp.lua
@@ -24,53 +24,58 @@ return {
       "hrsh7th/cmp-nvim-lsp",
       "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-path",
+      "hrsh7th/cmp-nvim-lua",
       "saadparwaiz1/cmp_luasnip",
       "L3MON4D3/LuaSnip",
     },
     opts = function()
       local cmp = require("cmp")
+      local cmp_action = require("lsp-zero").cmp_action()
       local lsnip_ok, luasnip = pcall(require, "luasnip")
       if not lsnip_ok then return end
       local lspkind_ok, lspkind = pcall(require, "lspkind")
       require("luasnip.loaders.from_vscode").lazy_load()
 
       return {
-        preselect = cmp.PreselectMode.None,
-        duplicates = {
-          nvim_lsp = 1,
-          luasnip = 1,
-          cmp_tabnine = 1,
-          buffer = 1,
-          path = 1,
-        },
+        -- Adding borders to completion menu
         window = {
           completion = cmp.config.window.bordered({ border = "rounded" }),
           documentation = cmp.config.window.bordered({ border = "rounded" }),
         },
+
+        -- Make the first item in completion menu always be selected.
+        preselect = "item",
         completion = {
           completeopt = "menu,menuone,noinsert",
         },
+
         snippet = {
           expand = function(args) luasnip.lsp_expand(args.body) end,
         },
         mapping = cmp.mapping.preset.insert({
-          ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-          ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<C-e>"] = cmp.mapping.abort(),
-          ["<Tab>"] = cmp.mapping.confirm({ select = true }),
+          -- Confirm completion
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          -- Scroll up and down the documentation window
+          ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-d>"] = cmp.mapping.scroll_docs(4),
+          ["<C-f>"] = cmp_action.luasnip_jump_forward(),
+          ["<C-b>"] = cmp_action.luasnip_jump_backward(),
+          ["<Tab>"] = cmp_action.luasnip_supertab(),
+          ["<S-Tab>"] = cmp_action.luasnip_shift_supertab(),
         }),
         sources = cmp.config.sources({
-          { name = "nvim_lsp", priority = 1000 },
-          { name = "luasnip", priority = 750 },
-          { name = "buffer", priority = 500 },
-          { name = "path", priority = 250 },
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "buffer" },
+          { name = "path" },
+          { name = "nvim_lua" },
         }),
         formatting = {
           fields = { "abbr", "menu", "kind" },
-          format = lspkind_ok and lspkind.cmp_format(),
+          format = lspkind_ok and lspkind.cmp_format({
+            maxwidth = 50,
+            ellipsis_char = "...",
+          }),
         },
         experimental = {
           ghost_text = true,
