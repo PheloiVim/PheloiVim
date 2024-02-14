3ebd438987801c60ec08b9cde65791fbd8b88cee
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-02-14 15:33:49 +0700

    fix(bufferline): call bufferline when restoring a session

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -32,4 +32,13 @@ return {
       },
     },
   },
+  config = function(_, opts)
+    require("bufferline").setup(opts)
+    -- Fix bufferline when restoring a session
+    vim.api.nvim_create_autocmd("BufAdd", {
+      callback = function()
+        vim.schedule(function() pcall(nvim_bufferline) end)
+      end,
+    })
+  end,
 }
