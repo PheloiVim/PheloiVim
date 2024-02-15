8a350a3d0a275b87893a55c8f9f37cf9b5922f9f
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-02-15 13:12:30 +0700

    chore: update `nvim-dap` config

--- lua/coding/nvim-dap.lua
+++ lua/coding/nvim-dap.lua
@@ -93,10 +93,7 @@ return {
 
   config = function()
     local icons = require("pheloivim.icons")
-    vim.api.nvim_set_hl(0, "DapStoppedLine", { default = true, link = "Visual" })
-
     for name, sign in pairs(icons.dap) do
-      sign = type(sign) == "table" and sign or { sign }
       vim.fn.sign_define(
         "Dap" .. name,
         { text = sign[1], texthl = sign[2] or "DiagnosticInfo", linehl = sign[3], numhl = sign[3] }
