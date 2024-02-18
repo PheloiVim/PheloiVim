8293100a833f650370e14ee360bcaf72d3317410
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-02-18 13:52:48 +0700

    chore: format code with stylua

--- lua/utils.lua
+++ lua/utils.lua
@@ -8,10 +8,7 @@ end
 function M.disable(server)
   local configs = require("lspconfig.configs")
   local def = rawget(configs, server)
-  def.document_config.on_new_config = require("lspconfig.util").add_hook_before(
-    def.document_config.on_new_config,
-    function(config) config.enabled = false end
-  )
+  def.document_config.on_new_config = require("lspconfig.util").add_hook_before(def.document_config.on_new_config, function(config) config.enabled = false end)
 end
 
 return M
