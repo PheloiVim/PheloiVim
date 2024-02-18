eefd1b3ae62fc607d1164596a932b99bf01d2905
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-02-18 09:01:57 +0700

    feat: add autocmds

---
+++ lua/pheloivim/autocmds.lua
@@ -0,0 +1,64 @@
+local autocmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
+
+autocmd({ "FocusGained", "TermClose", "TermLeave" }, {
+  desc = "Check if the file changed on editor focus",
+  group = augroup("checktime", { clear = true }),
+  command = "checktime",
+})
+
+autocmd("VimResized", {
+  desc = "Resize splits if window got resized",
+  group = augroup("resize_splits", { clear = true }),
+  callback = function()
+    local current_tab = vim.fn.tabpagenr()
+    vim.cmd("tabdo wincmd =")
+    vim.cmd("tabnext " .. current_tab)
+  end,
+})
+
+autocmd("BufReadPost", {
+  desc = "Go to last loc when opening a buffer",
+  group = augroup("last_loc", { clear = true }),
+  callback = function(args)
+    local exclude = { "gitcommit" }
+    local buf = args.buf
+    if vim.tbl_contains(exclude, vim.bo[buf].filetype) or vim.b[buf].last_loc then return end
+    vim.b[buf].last_loc = true
+    local mark = vim.api.nvim_buf_get_mark(buf, '"')
+    local lcount = vim.api.nvim_buf_line_count(buf)
+    if mark[1] > 0 and mark[1] <= lcount then pcall(vim.api.nvim_win_set_cursor, 0, mark) end
+  end,
+})
+
+autocmd("FileType", {
+  desc = "Unlist quickfist buffers",
+  group = augroup("unlist_quickfist", { clear = true }),
+  pattern = "qf",
+  callback = function() vim.opt_local.buflisted = false end,
+})
+
+autocmd("FileType", {
+  desc = "Close some file type with q",
+  group = augroup("close_with_q", { clear = true }),
+  pattern = { "help", "nofile", "qf", "diffview", "notify", "git" },
+  callback = function(args)
+    vim.keymap.set("n", "q", "<cmd>close<cr>", {
+      desc = "Close window",
+      buffer = args.buf,
+      silent = true,
+      nowait = true,
+    })
+    vim.opt_local.buflisted = false
+  end,
+})
+
+autocmd("BufWritePre", {
+  desc = "Auto create dir when saving a file, in case some intermediate directory does not exist",
+  group = augroup("auto_create_dir", { clear = true }),
+  callback = function(args)
+    if args.match:match("^%w%w+://") then return end
+    local file = vim.loop.fs_realpath(args.match) or args.match
+    vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
+  end,
+})
