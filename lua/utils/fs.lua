751b8afe0ef3fc35f5424114fc73494f304e2dfc
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-02-29 06:24:33 +0700

    chore(utils.fs): add comment

--- lua/utils/fs.lua
+++ lua/utils/fs.lua
@@ -1,6 +1,7 @@
 local M = {}
 
--- return root_dir
+-- return root directory
+-- `root_patterns` can be configured in `opts` of PheloiVim
 function M.root_dir()
   local root_dir = vim.fs.dirname(vim.fs.find(require("pheloivim").config.root_patterns, { upward = true, stop = vim.loop.os_homedir() })[1])
   return root_dir
