3fd8c5b2d21ede059a68b03d09e7f0f9f50bd428
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-02-28 14:20:04 +0700

    fix: force setup during initialization

--- lua/pheloivim/init.lua
+++ lua/pheloivim/init.lua
@@ -2,12 +2,14 @@ local M = {}
 
 local defaults = {
   colorscheme = "astrotheme",
+  mapleader = " ",
   modules = {
     options = true, -- pheloivim.options
     keymaps = true, -- pheloivim.keymaps
     autocmds = true, -- pheloivim.autocmds
   },
 }
+local config = {}
 
 M.did_init = false
 function M.init()
@@ -17,23 +19,26 @@ function M.init()
   local plugin = require("lazy.core.config").spec.plugins.PheloiVim
   if plugin then vim.opt.rtp:append(plugin.dir) end
 
-  vim.g.mapleader = " "
-end
+  -- force setup during initialization
+  local opts = require("lazy.core.plugin").values(plugin, "opts")
+  config = vim.tbl_deep_extend("force", defaults, opts or {}) or {}
 
-function M.setup(opts)
-  local config = vim.tbl_deep_extend("force", defaults, opts or {}) or {}
-
-  -- load modules
   for module, enabled in pairs(config.modules) do
     if enabled then
       local ok, err = pcall(require, "pheloivim." .. module)
-      if not ok then error(("Error loading module `%s`...\n\n%s"):format(module, err)) end
+      if not ok then error(("Error loading %s...\n\n%s"):format(module, err)) end
     end
   end
 
-  -- load colorscheme
+  vim.g.mapleader = config.mapleader
+end
+
+function M.setup()
   vim.api.nvim_create_autocmd("VimEnter", {
-    callback = function() vim.cmd.colorscheme(config.colorscheme) end,
+    callback = function()
+      -- load colorscheme
+      vim.cmd.colorscheme(config.colorscheme)
+    end,
   })
 end
 
