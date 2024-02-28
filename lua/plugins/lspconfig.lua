1591c8b65f377eb74d0cd654dae42a48dc208f02
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-02-28 22:05:38 +0700

    feat(lspconfig): add basic lsp setup

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -11,4 +11,52 @@ return {
       vim.g.lsp_zero_ui_float_border = 0
     end,
   },
+
+  {
+    "neovim/nvim-lspconfig",
+    event = { "BufReadPost", "BufNewFile" },
+    dependencies = {
+      "VonHeikemen/lsp-zero.nvim",
+      "williamboman/mason-lspconfig.nvim",
+      "williamboman/mason.nvim",
+    },
+    opts = {
+      -- options for vim.diagnostic.config()
+      diagnostic = {
+        underline = true,
+        update_in_insert = true,
+        severity_sort = true,
+      },
+      servers = {
+        lua_ls = {}, --TODO: remove later
+      },
+      setup = {},
+    },
+    config = function(_, opts)
+      local lsp_zero = require("lsp-zero")
+      lsp_zero.extend_lspconfig()
+
+      vim.diagnostic.config(opts.diagnostic)
+
+      -- Setup server
+      local ensure_installed = {}
+      local handlers = { lsp_zero.default_setup }
+      for server, server_opts in pairs(opts.servers) do
+        vim.list_extend(ensure_installed, { server })
+        if opts.setup[server] then
+          if opts.setup[server](server_opts) then handlers = vim.tbl_deep_extend("force", handlers, {
+            [server] = lsp_zero.noop,
+          }) end
+        else
+          handlers = vim.tbl_deep_extend("force", handlers, {
+            [server] = function() require("lspconfig")[server].setup(server_opts) end,
+          })
+        end
+      end
+      require("mason-lspconfig").setup({
+        ensure_installed = ensure_installed,
+        handlers = handlers,
+      })
+    end,
+  },
 }
