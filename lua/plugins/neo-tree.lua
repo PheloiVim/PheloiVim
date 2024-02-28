8207614f2873a857644344df06afe3cb0af122a9
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-02-28 13:42:50 +0700

    feat: add neo-tree

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,101 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+  },
+  cmd = "Neotree",
+  keys = {
+    {
+      "<leader>e",
+      function() require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd() }) end,
+      desc = "Neotree (cwd)",
+    },
+    {
+      "<leader>E",
+      function()
+        require("neo-tree.command").execute({
+          toggle = true,
+          dir = require("utils.fs").root_dir(".git"),
+        })
+      end,
+      desc = "Neotree (root)",
+    },
+  },
+  init = function()
+    local stats = vim.loop.fs_stat(vim.api.nvim_buf_get_name(0))
+    if stats and stats.type == "directory" then require("neo-tree") end
+  end,
+  opts = function()
+    local icons = require("icons")
+
+    return {
+      auto_clean_after_session_restore = true,
+      close_if_last_window = true,
+      sources = { "filesystem" },
+      default_component_configs = {
+        indent = {
+          padding = 0,
+          expander_collapsed = "",
+          expander_expanded = "",
+        },
+        icon = {
+          folder_closed = "",
+          folder_open = "",
+          folder_empty = "",
+          folder_empty_open = "",
+          default = "󰈙",
+        },
+        modified = { symbol = "" },
+        git_status = {
+          symbols = icons.git,
+        },
+      },
+      commands = {
+        parent_or_close = function(state)
+          local node = state.tree:get_node()
+          if node:has_children() and node:is_expanded() then
+            state.commands.toggle_node(state)
+          else
+            require("neo-tree.ui.renderer").focus_node(state, node:get_parent_id())
+          end
+        end,
+        child_or_open = function(state)
+          local node = state.tree:get_node()
+          if node:has_children() then
+            if not node:is_expanded() then -- if unexpanded, expand
+              state.commands.toggle_node(state)
+            else -- if expanded and has children, select the next child
+              if node.type == "file" then
+                state.commands.open(state)
+              else
+                require("neo-tree.ui.renderer").focus_node(state, node:get_child_ids()[1])
+              end
+            end
+          else -- if has no children
+            state.commands.open(state)
+          end
+        end,
+      },
+      window = {
+        width = 30,
+        mappings = {
+          ["<Space>"] = false, -- disable space until we figure out which-key disabling
+          h = "parent_or_close",
+          l = "child_or_open",
+        },
+      },
+      filesystem = {
+        follow_current_file = { enabled = true },
+        hijack_netrw_behavior = "open_current",
+        use_libuv_file_watcher = vim.fn.has("win32") ~= 1,
+      },
+      event_handlers = {
+        {
+          event = "neo_tree_buffer_enter",
+          handler = function(_) vim.opt_local.signcolumn = "auto" end,
+        },
+      },
+    }
+  end,
+}
