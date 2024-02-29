ce7507d58a4116c6d5f0d2fc75235ddb7fc56350
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-02-29 16:26:58 +0700

    feat: add autopairs

---
+++ lua/plugins/autopairs.lua
@@ -0,0 +1,24 @@
+return {
+  "windwp/nvim-autopairs",
+  event = "InsertEnter",
+  opts = {
+    disable_filetype = { "TelescopePrompt" },
+    check_ts = true,
+    fast_wrap = {
+      map = "<M-e>",
+      chars = { "{", "[", "(", '"', "'" },
+      pattern = ([[ [%'%"%)%>%]%)%}%,] ]]):gsub("%s+", ""),
+      offset = 0,
+      end_key = "$",
+      keys = "qwertyuiopzxcvbnmasdfghjkl",
+      check_comma = true,
+    },
+  },
+  config = function(_, opts)
+    require("nvim-autopairs").setup(opts)
+    require("cmp").event:on(
+      "confirm_done",
+      require("nvim-autopairs.completion.cmp").on_confirm_done({ tex = false })
+    )
+  end,
+}
