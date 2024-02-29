08f2bf1fe7b689c9dc596dfec39ed1260c7bb5de
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-02-29 13:52:20 +0700

    feat: add which-key

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,1 +1,31 @@
-return {}
+return {
+  "folke/which-key.nvim",
+  keys = { "<leader>", "z", "d", "c", "v", "g", "]", "[", "y", "'" },
+  opts = {
+    window = {
+      border = "rounded",
+    },
+    show_help = false,
+    show_keys = false,
+    plugins = { spelling = true },
+    icons = { group = "" },
+    defaults = {
+      b = { name = "󰓩 Buffer" },
+      c = { name = " Code" },
+      f = { name = "󰈔 Find/File" },
+      s = { name = " Search" },
+      u = { name = "󰓩 UI" },
+      w = { name = " Windows" },
+      x = { name = " Diagnostics/Quickfix" },
+      d = { name = " Debug" },
+      t = { name = "󰙨 Test" },
+      g = { name = "󰊢 Git", h = "Hunks" },
+      p = { name = " Packages" },
+    },
+  },
+  config = function(_, opts)
+    local wk = require("which-key")
+    wk.setup(opts)
+    wk.register(opts.defaults, { prefix = "<leader>", mode = { "n", "v" } })
+  end,
+}
