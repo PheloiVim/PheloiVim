3a2562379d170acb32ec710fb9e47c7a8eb1c8af
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-02-29 23:26:18 +0700

    chore: update comment

--- lua/utils/manager.lua
+++ lua/utils/manager.lua
@@ -3,7 +3,7 @@ local M = {}
 --- Checks if a plugin is available.
 --- This function checks if a specified plugin is available in the `lazy.nvim` configuration.
 --- @param plugin string: The name of the plugin to check for availability.
---- @return boolean: True if the plugin is available, false otherwise.
+--- @return boolean: true if the plugin is available, false otherwise.
 function M.has(plugin) return require("lazy.core.config").spec.plugins[plugin] ~= nil end
 
 return M
