4b1fcc7b9a79bdee7de1d3c877fd7b5dc204f2f8
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-02-29 13:51:55 +0700

    feat(catppuccin): enable more integrations

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -7,6 +7,26 @@ return {
     flavour = "mocha",
     integrations = {
       cmp = true,
+      native_lsp = {
+        enabled = true,
+        virtual_text = {
+          errors = { "italic" },
+          hints = { "italic" },
+          warnings = { "italic" },
+          information = { "italic" },
+        },
+        underlines = {
+          errors = { "underline" },
+          hints = { "underline" },
+          warnings = { "underline" },
+          information = { "underline" },
+        },
+        inlay_hints = {
+          background = true,
+        },
+      },
+      which_key = true,
+      gitsigns = true,
     },
   },
 }
