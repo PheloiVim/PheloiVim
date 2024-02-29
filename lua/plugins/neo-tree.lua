9788b31e7be623bec60a981304b1160acd3cf72f
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-02-29 23:39:18 +0700

    chore(neo-tree): add some comments

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -24,8 +24,14 @@ return {
     },
   },
   init = function()
+    -- Get file system statistics for the current buffer
     local stats = vim.uv.fs_stat(vim.api.nvim_buf_get_name(0))
-    if stats and stats.type == "directory" then require("neo-tree") end
+
+    -- Check if stats exist and if the type is a directory
+    if stats and stats.type == "directory" then
+      -- If the current buffer represents a directory, initialize the Neo Tree plugin
+      require("neo-tree")
+    end
   end,
   opts = {
     auto_clean_after_session_restore = true,
@@ -43,26 +49,41 @@ return {
     },
     commands = {
       parent_or_close = function(state)
+        -- Get the current node from the state tree
         local node = state.tree:get_node()
+
+        -- Check if the current node has children and is expanded
         if node:has_children() and node:is_expanded() then
+          -- If the node has children and is expanded, toggle the node (collapse it)
           state.commands.toggle_node(state)
         else
+          -- If the node does not have children or is not expanded,
+          -- focus on its parent node by calling the focus_node function from the renderer module
           require("neo-tree.ui.renderer").focus_node(state, node:get_parent_id())
         end
       end,
       child_or_open = function(state)
+        -- Get the current node from the state tree
         local node = state.tree:get_node()
+
+        -- Check if the current node has children
         if node:has_children() then
-          if not node:is_expanded() then -- if unexpanded, expand
+          -- If the node has children, check if it's expanded
+          if not node:is_expanded() then
+            -- If the node is not expanded, toggle the node to expand it
             state.commands.toggle_node(state)
-          else -- if expanded and has children, select the next child
+          else
+            -- If the node is expanded, check if it's a file or directory node
             if node.type == "file" then
+              -- If it's a file node, open the file
               state.commands.open(state)
             else
+              -- If it's a directory node, focus on the first child node
               require("neo-tree.ui.renderer").focus_node(state, node:get_child_ids()[1])
             end
           end
-        else -- if has no children
+        else
+          -- If the node does not have children, simply open the file (if it's a file node)
           state.commands.open(state)
         end
       end,
