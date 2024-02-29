112e0d44f7a09306c1bf4241ef69d465435dfb7c
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-02-29 12:46:45 +0700

    feat(lsp): add lspsaga

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -43,6 +43,22 @@ return {
       -- Diagnostic
       vim.diagnostic.config(opts.diagnostic)
 
+      -- Setup keymaps
+      lsp_zero.on_attach(function(_, bufnr)
+        lsp_zero.default_keymaps({
+          buffer = bufnr,
+          exclude = { "gd", "<F2>", "<F3>", "<F4>", "gl", "[d", "]d" },
+        })
+        vim.keymap.set("n", "<leader>ca", function() vim.cmd("Lspsaga code_action") end, { desc = "Code action" })
+        vim.keymap.set("n", "<leader>cp", function() vim.cmd("Lspsaga peek_definition") end, { desc = "Peek definition" })
+        vim.keymap.set("n", "<leader>cr", function() vim.cmd("Lspsaga rename") end, { desc = "LSP rename" })
+        vim.keymap.set("n", "<leader>co", function() vim.cmd("Lspsaga outline") end, { desc = "Code outline" })
+        vim.keymap.set("n", "]d", function() vim.cmd("Lspsaga diagnostic_jump_next") end, { desc = "Next diagnostic" })
+        vim.keymap.set("n", "[d", function() vim.cmd("Lspsaga diagnostic_jump_prev") end, { desc = "Previous diagnostic" })
+        vim.keymap.set("n", "gd", function() vim.cmd("Lspsaga goto_definition") end, { desc = "Go to definition" })
+        vim.keymap.set("n", "<leader>li", function() vim.cmd("LspInfo") end, { desc = "Lsp Info" })
+      end)
+
       -- Setup server
       local ensure_installed = {}
       local handlers = { lsp_zero.default_setup }
