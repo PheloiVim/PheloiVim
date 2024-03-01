f6dddcc9acfff5d5161187988ca8342d50e37871
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-03-01 16:13:53 +0700

    feat(mason): auto install tools

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -2,6 +2,7 @@ return {
   "williamboman/mason.nvim",
   cmd = "Mason",
   opts = {
+    ensure_installed = { "stylua" },
     ui = {
       icons = {
         package_installed = "✓",
@@ -10,4 +11,21 @@ return {
       },
     },
   },
+  config = function(_, opts)
+    require("mason").setup(opts)
+
+    require("mason-registry"):on("package:install:success", function()
+      vim.defer_fn(function()
+        -- trigger FileType event to possibly load this newly installed LSP server
+        require("lazy.core.handler.event").trigger({
+          event = "FileType",
+          buf = vim.api.nvim_get_current_buf(),
+        })
+      end, 100)
+    end)
+
+    for _, tool in ipairs(opts.ensure_installed) do
+      require("utils.manager").install_package(tool)
+    end
+  end,
 }
--- lua/utils/manager.lua
+++ lua/utils/manager.lua
@@ -8,4 +8,37 @@ function M.has(plugin)
   return require("lazy.core.config").spec.plugins[plugin] ~= nil
 end
 
+--- Install a package using Mason
+---@param package_name string
+function M.install_package(package_name)
+  local registry = require("mason-registry")
+  local optional = require("mason-core.optional")
+  local p = require("mason-core.package")
+  local source = p.Parse(package_name)
+
+  optional
+    .of_nilable(source)
+    :map(function(source_name)
+      local ok, pkg = pcall(registry.get_package, source_name)
+      if ok then return pkg end
+    end)
+    :if_present(function(pkg)
+      if not pkg:is_installed() then
+        vim.notify(
+          ("[pheloivim-mason] installing %s"):format(pkg.name),
+          vim.log.levels.INFO
+        )
+        pkg:install():once(
+          "closed",
+          function()
+            vim.notify(
+              ("[pheloivim-mason] %s was installed"):format(pkg.name),
+              vim.log.levels.INFO
+            )
+          end
+        )
+      end
+    end)
+end
+
 return M
