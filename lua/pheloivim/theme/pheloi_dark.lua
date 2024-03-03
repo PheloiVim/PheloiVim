c373b8d33b72145e65dd4b4567b5830f26ae14ef
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-03-03 13:49:15 +0700

    chore: add todo list

---
+++ lua/pheloivim/theme/pheloi_dark.lua
@@ -0,0 +1,9 @@
+-- TODO: Implement default dark theme for PheloiVim here
+
+local M = {}
+
+function M.load()
+	print("pheloi_dark")
+end
+
+return M
---
+++ lua/pheloivim/theme/pheloi_light.lua
@@ -0,0 +1,9 @@
+-- TODO: Implement default light theme for PheloiVim here
+
+local M = {}
+
+function M.load()
+	print("pheloi_light")
+end
+
+return M
