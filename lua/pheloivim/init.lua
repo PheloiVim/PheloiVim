b74aeb139d9c643433d174099e99fe02f3d2c048
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-03-03 16:01:13 +0700

    I copied code from LazyVim and now it's ok

--- lua/pheloivim/init.lua
+++ lua/pheloivim/init.lua
@@ -1,9 +1,80 @@
+---@class PheloiVimConfig: PheloiVimOptions
 local M = {}
 
-M.config = {}
+--- Default configuration options
+---@class PheloiVimOptions
+local defaults = {
+  colorscheme = "pheloi_dark",
+  modules = {
+    autocmds = true,
+    keymaps = true,
+  },
+}
 
-function M.init() end
+--- Current configuration options
+---@type PheloiVimOptions
+local options
 
-function M.setup() end
+--- Setup PheloiVim configuration
+---@param opts? PheloiVimOptions: Options for PheloiVim (optional)
+function M.setup(opts)
+  options = vim.tbl_deep_extend("force", defaults, opts or {}) or {}
+
+  local lazy_autocmds = vim.fn.argc(-1) == 0
+  if not lazy_autocmds then M.load("autocmds") end
+
+  local group = vim.api.nvim_create_augroup("PheloiVim", { clear = true })
+  vim.api.nvim_create_autocmd("User", {
+    group = group,
+    pattern = "VeryLazy",
+    callback = function()
+      if lazy_autocmds then M.load("autocmds") end
+      M.load("keymaps")
+    end,
+  })
+
+  if type(M.colorscheme) == "function" then
+    M.colorscheme()
+  else
+    vim.cmd.colorscheme(M.colorscheme)
+  end
+end
+
+--- Load configurations for a specific module
+---@param name "autocmds" | "options" | "keymaps"
+function M.load(name)
+  local function _load(mod)
+    if require("lazy.core.cache").find(mod)[1] then require(mod) end
+  end
+
+  if M.modules[name] or name == "options" then _load("pheloivim.core." .. name) end
+
+  _load("config." .. name)
+
+  if vim.bo.filetype == "lazy" then vim.cmd("do VimResized") end
+
+  local pattern = "PheloiVim" .. name:sub(1, 1):upper() .. name:sub(2)
+  vim.api.nvim_exec_autocmds("User", { pattern = pattern, modeline = false })
+end
+
+M.did_init = false
+
+--- Initialize PheloiVim
+function M.init()
+  if M.did_init then return end
+  M.did_init = true
+  local plugin = require("lazy.core.config").spec.plugins.PheloiVim
+  if plugin then vim.opt.rtp:append(plugin.dir) end
+  M.load("options")
+end
+
+--- Metatable to provide default values when accessing M
+---@type table<string, function>
+setmetatable(M, {
+  __index = function(_, key)
+    if options == nil then return vim.deepcopy(defaults)[key] end
+    return options[key]
+  end,
+})
 
 return M
