90b33fbc136b491f9e93b23741d5c99f38598b8a
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-03-05 15:48:39 +0700

    chore: format code with stylua

--- lua/utils/fs.lua
+++ lua/utils/fs.lua
@@ -4,8 +4,8 @@ local M = {}
 --- @param dir_patterns string|table: A table containing one or more directory patterns to search for.
 --- @return string|nil: The path of the first matching directory, or `nil` if no match is found.
 function M.find_dir(dir_patterns)
- local dir = vim.fs.dirname(vim.fs.find(dir_patterns, { upward = true, stop = vim.loop.os_homedir() })[1])
- return dir
+  local dir = vim.fs.dirname(vim.fs.find(dir_patterns, { upward = true, stop = vim.loop.os_homedir() })[1])
+  return dir
 end
 
 return M
