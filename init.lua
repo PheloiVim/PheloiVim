307807752b2423cfed165ad2e01ea549ae2b9cc6
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-03-07 13:10:29 +0700

    chore(main): remove comment

--- init.lua
+++ init.lua
@@ -1,18 +1,14 @@
--- Display messages in the command line with different highlight groups
 vim.api.nvim_echo({
   {
     "This repository is not meant to be used as a direct Neovim configuration\n",
     "ErrorMsg",
-  }, -- Display an error message
+  },
   {
     "Please check the PheloiVim documentation for installation details\n",
     "WarningMsg",
-  }, -- Display a warning message
-  { "Press any key to exit...", "MoreMsg" }, -- Display a message prompting to press any key
-}, true, {}) -- The `true` argument clears the command line before displaying messages, `{}` is used for options which are empty in this case
+  },
+  { "Press any key to exit...", "MoreMsg" },
+}, true, {})
 
--- Wait for a single character input from the user
 vim.fn.getchar()
-
--- Quit Neovim
 vim.cmd.quit()
