ae1a2d8440a651f8322624f2b65146efdc5ace0e
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-03-07 21:57:13 +0700

    I'm lazy and switched to LazyVim

---
+++ README.md
@@ -0,0 +1,38 @@
+# LazyVim Configuration
+
+My Neovim configuration built on LazyVim to empower your editing experience! This configuration prioritizes both efficiency and customization, allowing you to tailor your development environment to your specific needs.
+
+**Features:**
+
+- **Effortless Setup:** Leverage the power of LazyVim for a streamlined installation and configuration process.
+- **Customizable:** Tweak various aspects of Neovim to match your workflow preferences.
+- **Optimized for:** Replace with your specific use cases (e.g., web development, writing)
+
+**Installation:**
+
+**Prerequisites:**
+
+- **Neovim:** Ensure you have Neovim installed on your system.
+
+**Steps:**
+
+1. **Clone this repository:**
+
+   ```bash
+   git clone https://github.com/2giosangmitom/neovim-config ~/.config/nvim
+   ```
+
+2. Open Neovim
+
+   ```bash
+   nvim
+   ```
+
+**Customization:**
+
+This configuration utilizes Lua for customization. Explore the files within the repository (`~/.config/nvim`) to understand the current settings. Modify these files to personalize your Neovim experience.
+
+**Further Information:**
+
+- **LazyVim Documentation:** [https://www.lazyvim.org/](https://www.lazyvim.org/)
+- **Feel free to reach out with any questions or suggestions!**
--- init.lua
+++ init.lua
@@ -1,55 +1,23 @@
-require("core.options")
-require("core.autocmds")
-require("core.keymaps")
-require("bootstrap").lazy()
-
-local icons = require("icons")
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+end
+vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
 require("lazy").setup({
-  defaults = { lazy = true },
   spec = {
+    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     { import = "plugins" },
   },
-  ui = {
-    icons = {
-      ft = "",
-      loaded = icons.check,
-      not_loaded = icons.circle,
-    },
+  defaults = {
+    lazy = true,
+    version = false, -- always use the latest git commit
   },
-  install = {
-    colorscheme = { "tokyodark" },
-  },
+  install = { colorscheme = { "tokyonight", "habamax" } },
+  checker = { enabled = true }, -- automatically check for plugin updates
   performance = {
     rtp = {
       disabled_plugins = {
-        "2html_plugin",
-        "tohtml",
-        "getscript",
-        "getscriptPlugin",
-        "gzip",
-        "logipat",
-        "netrw",
-        "netrwPlugin",
-        "netrwSettings",
-        "netrwFileHandlers",
-        "matchit",
-        "tar",
-        "tarPlugin",
-        "rrhelper",
-        "spellfile_plugin",
-        "vimball",
-        "vimballPlugin",
-        "zip",
-        "zipPlugin",
-        "tutor",
-        "rplugin",
-        "syntax",
-        "synmenu",
-        "optwin",
-        "compiler",
-        "bugreport",
-        "ftplugin",
       },
     },
   },
