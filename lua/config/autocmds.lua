81ec79d1101ce9da91e347394ef4dfe6e776fc9f
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-03-08 20:08:32 +0700

    fix(autocmds): exclude `help` auto disable/enable relative line

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,13 +1,26 @@
 -- Disable relative number in command mode
 vim.api.nvim_create_autocmd("CmdLineEnter", {
 	callback = function()
-		local excluded_filetypes = { "lazy" }
+		local excluded_filetypes = { "lazy", "help" }
 		local ft = vim.bo.filetype
 		if vim.tbl_contains(excluded_filetypes, ft) then
 			return
 		end
 
 		vim.opt_local.relativenumber = false
+	end,
+})
+
+-- Reset relative number after leaving command mode
+vim.api.nvim_create_autocmd("CmdLineLeave", {
+	callback = function()
+		local excluded_filetypes = { "lazy", "help" }
+		local ft = vim.bo.filetype
+		if vim.tbl_contains(excluded_filetypes, ft) then
+			return
+		end
+
+		vim.opt_local.relativenumber = true
 	end,
 })
 
@@ -16,18 +29,5 @@ vim.api.nvim_create_autocmd("FileType", {
 	pattern = { "qf" },
 	callback = function()
 		vim.opt_local.relativenumber = false
-	end,
-})
-
--- Reset relative number after leaving command mode
-vim.api.nvim_create_autocmd("CmdLineLeave", {
-	callback = function()
-		local excluded_filetypes = { "lazy" }
-		local ft = vim.bo.filetype
-		if vim.tbl_contains(excluded_filetypes, ft) then
-			return
-		end
-
-		vim.opt_local.relativenumber = true
 	end,
 })
