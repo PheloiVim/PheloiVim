e478a7a1e05523d76bfa7c9acacc8c8d0812d9f3
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-03-08 14:24:44 +0700

    feat(keymaps): move in insert mode

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,7 @@
+local map = vim.keymap.set
+
+-- Move in insert mode
+map("i", "<C-h>", "<Left>", { desc = "Move Left" })
+map("i", "<C-l>", "<Right>", { desc = "Move Right" })
+map("i", "<C-j>", "<Down>", { desc = "Move Down" })
+map("i", "<C-k>", "<Up>", { desc = "Move Up" })
