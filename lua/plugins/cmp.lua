c2f5f21089293f7522c96bc4d30d3e2a6abd5f02
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-03-08 12:38:42 +0700

    feat(cmp): bordered completion window

---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,15 @@
+return {
+	"hrsh7th/nvim-cmp",
+	opts = function(_, opts)
+		local cmp = require("cmp")
+
+		opts.window = {
+			completion = cmp.config.window.bordered(),
+			documentation = cmp.config.window.bordered(),
+		}
+
+		opts.experimental = {
+			ghost_text = false,
+		}
+	end,
+}
