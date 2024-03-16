fbd761865cd02ebcc01c493404170c36ac117342
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-03-16 08:17:46 +0700

    feat: add flake.nix

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "My Neovim configuration, built on top of the LazyVim";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+        packages = with pkgs; [
+          rustc
+          cargo
+          clippy
+          dotnet-sdk_8
+          fish
+          nodejs_21
+          go
+          lua
+        ];
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = packages;
+          shellHook = ''
+            exec fish
+          '';
+        };
+        formatter = pkgs.alejandra;
+      }
+    );
+}
